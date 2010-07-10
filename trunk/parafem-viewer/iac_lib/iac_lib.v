4e61ae56495ac026ea6922c1dcf7b671b6512ac4
louise.lever@manchester.ac.uk <louise.lever@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2010-07-10 16:27:19 +0000

    Added AVS/Express derived project infrastructure base files, for adding IAC style projects and library references. Includes ParaFEM-Viewer specific content.

---
+++ trunk/parafem-viewer/iac_lib/iac_lib.v
@@ -0,0 +1,13 @@
+// INTERNATIONAL AVS CENTRE - WARRANTY DISCLAIMER
+// Please read the file DISCLAIMER for conditions associated with this file.
+// avs@iavsc.org, www.iavsc.org
+
+flibrary IAC<user_library=0,compile_subs=0> {
+  "../iac_lib/common.v" COMMON;
+  "../iac_lib/data_io.v" DATA_IO;
+  "../iac_lib/filters.v" FILTERS;
+  "../iac_lib/mappers.v" MAPPERS;
+  "../iac_lib/glyphs.v" GLYPHS;
+  "../iac_lib/misc.v" MISC;
+  "../iac_lib/examples.v" EXAMPLES;
+};
---
+++ trunk/parafem-viewer/v/proc.v
@@ -0,0 +1,5 @@
+// INTERNATIONAL AVS CENTRE - WARRANTY DISCLAIMER
+// Please read the file DISCLAIMER for conditions associated with this file.
+// avs@iavsc.org, www.iavsc.org
+
+"$XP_PATH<0>/v/proc.v" ProcTemplates;
