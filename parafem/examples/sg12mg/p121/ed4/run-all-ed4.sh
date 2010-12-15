f2b960e3fb3524db0b0237b17ef27d38c7df31ad
lee.margetts <lee.margetts@manchester.ac.uk>
2010-12-15 23:15:09 +0000

    Added shell script run-all-ed4.sh and updated readme.txt

---
+++ parafem/examples/sg12mg/p121/ed4/run-all-ed4.sh
@@ -0,0 +1,8 @@
+sg12mg ed4-tiny-c3d8
+sg12mg ed4-small-c3d8
+sg12mg ed4-medium-c3d8
+sg12mg ed4-large-c3d8
+sg12mg ed4-tiny-c3d20
+sg12mg ed4-small-c3d20
+sg12mg ed4-medium-c3d20
+sg12mg ed4-large-c3d20
