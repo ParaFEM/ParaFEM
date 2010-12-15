48fe87479c41d51ec810791612c4267c9705ff7a
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2010-12-15 23:19:46 +0000

    Added shell script run-all-bsq.sh and updated readme.txt

---
+++ trunk/parafem/examples/sg12mg/p121/boussinesq/run-all-bsq.sh
@@ -0,0 +1,8 @@
+sg12mg bsq-tiny-c3d8
+sg12mg bsq-small-c3d8
+sg12mg bsq-medium-c3d8
+sg12mg bsq-large-c3d8
+sg12mg bsq-tiny-c3d20
+sg12mg bsq-small-c3d20
+sg12mg bsq-medium-c3d20
+sg12mg bsq-large-c3d20
