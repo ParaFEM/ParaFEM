0d0a20edef9b91fa8a0a7d29bed7c224573e6b60
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2011-02-17 11:32:27 +0000

    Added new example to /examples/sg12mg/p121/ed4/ - a macro to create a mesh with 64M elements.

--- trunk/parafem/examples/sg12mg/p121/ed4/run-all-ed4.sh
+++ trunk/parafem/examples/sg12mg/p121/ed4/run-all-ed4.sh
@@ -10,6 +10,9 @@ echo "sg12mg ed4-medium-c3d8"
 sg12mg ed4-large-c3d8
 echo "sg12mg ed4-large-c3d8"
 
+sg12mg ed4-giant-c3d8
+echo "sg12mg ed4-giant-c3d8"
+
 sg12mg ed4-tiny-c3d20
 echo "sg12mg ed4-tiny-c3d20"
 
@@ -21,4 +24,8 @@ echo "sg12mg ed4-medium-c3d20"
 
 sg12mg ed4-large-c3d20
 echo "sg12mg ed4-large-c3d20"
+
+sg12mg ed4-giant-c3d20
+echo "sg12mg ed4-giant-c3d20"
+
 
