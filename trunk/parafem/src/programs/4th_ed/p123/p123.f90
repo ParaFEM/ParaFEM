ec41ac01ae3a1a97a6ace503a2314ff4fd057c4f
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2012-03-07 11:36:48 +0000

    Modified mg2d.f90 to create input decks for the forthcoming version of p123.f90 that will permit I/O for externally generated models.
    Modified p123-small.mg to be consistent with read statements in mg2d.f90
    
    Further work, incomplete, on p123.f90 committed.

--- trunk/parafem/src/programs/4th_ed/p123/p123.f90
+++ trunk/parafem/src/programs/4th_ed/p123/p123.f90
@@ -20,89 +20,218 @@ PROGRAM p123
 
  !  neq , ntot  are now global variables - not declared
 
- INTEGER::nxe,nye,nze,nn,nr,nip,nodof=1,nod=8, nres, is , it ,       &
-          i,j,k,ndim=3,iters,limit,iel,num_no,no_index_start,        &
-          neq_temp,nn_temp , loaded_freedoms, fixed_freedoms,        &
-          nels,ndof,partitioner=1,ielpe,npes_pp
- REAL(iwp)::aa,bb,cc,kx,ky,kz,det,tol,up,alpha,beta,q,penalty=1.e20_iwp 
- REAL(iwp),PARAMETER :: zero = 0.0_iwp
- CHARACTER(LEN=15):: element= 'hexahedron';    LOGICAL :: converged          
- CHARACTER(LEN=50):: job_name
-!-------------------------- dynamic arrays-------------------------------------
- REAL(iwp),ALLOCATABLE :: points(:,:),kc(:,:),coord(:,:), weights(:),        &
-                         p_g_co_pp(:,:,:), jac(:,:), der(:,:), deriv(:,:),   &
-                         col(:,:),row(:,:),kcx(:,:),kcy(:,:),kcz(:,:),       &
-                         diag_precon_pp(:),p_pp(:),r_pp(:),x_pp(:),          &
-                         xnew_pp(:),u_pp(:),pmul_pp(:,:),utemp_pp(:,:),      &
-                         d_pp(:),diag_precon_tmp(:,:),val(:),val_f(:),       &
-                         store_pp(:),eld(:),timest(:)
- INTEGER, ALLOCATABLE :: rest(:,:),g(:),num(:),g_num_pp(:,:),g_g_pp(:,:),no(:),&
-                         no_f(:),no_local_temp(:),no_local_temp_f(:),no_local(:)
-!-------------------------input and initialisation---------------------------
+  INTEGER, PARAMETER    :: ndim=3,nodof=1,nod=8,partitioner=1
+  INTEGER               :: nn,nr,nip
+  INTEGER               :: nxe,nye,nze,nres,is,it
+  INTEGER               :: nn_temp,neq_temp
+  INTEGER               :: i,j,k,iters,limit,iel,num_no,no_index_start
+  INTEGER               :: loaded_freedoms,fixed_freedoms
+  INTEGER               :: nels,ndof,ielpe,npes_pp
+  REAL(iwp)             :: aa,bb,cc 
+  REAL(iwp)             :: kx,ky,kz,det,tol,up,alpha,beta,q
+  REAL(iwp),PARAMETER   :: zero = 0.0_iwp,penalty=1.e20_iwp
+  CHARACTER(LEN=15)     :: element= 'hexahedron'
+  CHARACTER(LEN=50)     :: job_name
+  LOGICAL               :: converged 
 
- ALLOCATE(timest(25))
- timest = zero
- timest(1) = elap_time( )
+!------------------------------------------------------------------------------
+! 2. Declare dynamic arrays
+!------------------------------------------------------------------------------
 
- CALL find_pe_procs(numpe,npes)
- IF (numpe==npes) THEN
-  OPEN (10,FILE='p123.dat',STATUS=    'OLD',ACTION='READ')
-  READ (10,*) nels,nxe,nze,nip,aa,bb,cc,kx,ky,kz, tol,limit ,             &
-              loaded_freedoms,fixed_freedoms
- END IF
- CALL bcast_inputdata_p123(numpe,npes,nels,nxe,nze,nip,aa,bb,cc,kx,ky,kz, &
-                            tol,limit,loaded_freedoms,fixed_freedoms)
- CALL calc_nels_pp(job_name,nels,npes,numpe,partitioner,nels_pp)
+  REAL(iwp),ALLOCATABLE :: points(:,:),kc(:,:),coord(:,:), weights(:)
+  REAL(iwp),ALLOCATABLE :: p_g_co_pp(:,:,:), jac(:,:), der(:,:), deriv(:,:)
+  REAL(iwp),ALLOCATABLE :: col(:,:),row(:,:),kcx(:,:),kcy(:,:),kcz(:,:)
+  REAL(iwp),ALLOCATABLE :: diag_precon_pp(:),p_pp(:),r_pp(:),x_pp(:)
+  REAL(iwp),ALLOCATABLE :: xnew_pp(:),u_pp(:),pmul_pp(:,:),utemp_pp(:,:)
+  REAL(iwp),ALLOCATABLE :: d_pp(:),diag_precon_tmp(:,:),val(:),val_f(:)
+  REAL(iwp),ALLOCATABLE :: store_pp(:),eld(:),timest(:)
+  INTEGER, ALLOCATABLE  :: rest(:,:),g(:),num(:),g_num_pp(:,:),g_g_pp(:,:),no(:)
+  INTEGER, ALLOCATABLE  :: no_f(:),no_local_temp(:),no_local_temp_f(:)
+  INTEGER, ALLOCATABLE  :: no_local(:)
+ 
+!------------------------------------------------------------------------------
+! 3. Read job_name from the command line. 
+!    Read control data, mesh data, boundary and loading conditions
+!------------------------------------------------------------------------------
 
- ndof=nod*nodof ; ntot=ndof ; nye = nels/nxe/nze  
-      neq_temp = 0; nn_temp = 0 ; nr=(nxe+1)*(nye+1)+(nxe+1)*nze+nye*nze  
- ALLOCATE (points(nip,ndim),coord(nod,ndim),jac(ndim,ndim),kc(ntot,ntot),    &
-           der(ndim,nod),deriv(ndim,nod),rest(nr,nodof+1),kcx(ntot,ntot),    &
-           g(ntot),pmul_pp(ntot,nels_pp),utemp_pp(ntot,nels_pp),col(ntot,1), &
-           p_g_co_pp(nod,ndim,nels_pp),g_num_pp(nod,nels_pp),weights(nip),   &
-           num(nod),g_g_pp(ntot,nels_pp),no(1),kcy(ntot,ntot),val(1),       &
-           no_local_temp(1),row(1,ntot),diag_precon_tmp(ntot,nels_pp),      &
-           val_f(1),eld(ntot),no_f(1),no_local_temp_f(1),kcz(ntot,ntot))
-      CALL box_bc8(rest,nxe,nye,nze); ielpe=iel_start; nres=nxe*(nze-1)+1
-      IF(loaded_freedoms>0) THEN; no = nres;   val = 10.; END IF
-      IF(fixed_freedoms>0)THEN; no_f=nres; val_f = 100. ; END IF 
-      CALL sample(element,points,weights); CALL rearrange_2(rest)  
-!---------- loop the elements for global cordinates etc ---------------------
-     elements_0: DO iel = 1 , nels_pp
-             CALL geometry_8bxz(ielpe,nxe,nze,aa,bb,cc,coord,num)
-             CALL find_g4(num,g,rest) ; g_num_pp(:,iel) = num
-             p_g_co_pp(:,:,iel) = coord; g_g_pp(:,iel) = g; ielpe = ielpe+1 
-             i = MAXVAL(g); j = MAXVAL(num)
-             IF(i>neq_temp)neq_temp = i; IF(j>nn_temp)nn_temp = j
-     END DO elements_0
-   neq = reduce(neq_temp)  ;  nn = reduce(nn_temp)
-   CALL calc_neq_pp          
-   CALL calc_npes_pp(npes,npes_pp)
-   CALL make_ggl2(npes_pp,npes,g_g_pp)
-   diag_precon_tmp = .0_iwp
+  ALLOCATE(timest(25))
+  timest = zero
+  timest(1) = elap_time()
+
+  CALL find_pe_procs(numpe,npes)
+
+! argc = iargc()
+! IF(argc /= 1) CALL job_name_error(numpe,program_name)
+! CALL GETARG(1,job_name)
+
+! CALL read_p123(job_name,numpe,fixed_freedoms,kx,ky,kz,limit,loaded_nodes,   &
+!                meshgen,nels,nip,nn,nod,nr,partitioner,tol,v)
+
+!-- remove
+
+  IF (numpe==npes) THEN
+   OPEN (10,FILE='p123.dat',STATUS=    'OLD',ACTION='READ')
+   READ (10,*) nels,nxe,nze,nip,aa,bb,cc,kx,ky,kz, tol,limit ,             &
+               loaded_freedoms,fixed_freedoms
+  END IF
+  CALL bcast_inputdata_p123(numpe,npes,nels,nxe,nze,nip,aa,bb,cc,kx,ky,kz, &
+                             tol,limit,loaded_freedoms,fixed_freedoms)
+!-- end remove
+
+  CALL calc_nels_pp(job_name,nels,npes,numpe,partitioner,nels_pp)
+
+  ndof = nod*nodof 
+  ntot = ndof 
+
+!-- remove
+
+  nye      = nels/nxe/nze  
+  neq_temp = 0 
+  nn_temp  = 0 
+  nr       = (nxe+1)*(nye+1)+(nxe+1)*nze+nye*nze  
+
+!-- end remove
+
+  ALLOCATE(g_num_pp(nod,nels_pp))
+! ALLOCATE(g_coord_pp(nod,ndim,nels_pp))
+  ALLOCATE(p_g_co_pp(nod,ndim,nels_pp))
+  ALLOCATE(rest(nr,nodof+1))
+
+  g_num_pp   = 0
+! g_coord_pp = zero
+  p_g_co_pp  = zero
+  rest       = 0
+
+  timest(2) = elap_time()
+
+! CALL read_g_num_pp2(job_name,iel_start,nn,npes,numpe,g_num_pp)
+  timest(3) = elap_time()
+
+! IF(meshgen == 2) CALL abaqus2sg(element,g_num_pp)
+  timest(4) = elap_time()
+
+! CALL read_g_coord_pp(job_name,g_num_pp,nn,npes,numpe,g_coord_pp)
+  timest(5) = elap_time()
+
+! CALL read_rest(job_name,numpe,rest)
+  timest(6) = elap_time()
+
+  IF(numpe==1) PRINT *, " *** Read input data in: ", timest(6)-timest(1)," s"
+
+!------------------------------------------------------------------------------
+! 4. Allocate dynamic arrays used in main program
+!------------------------------------------------------------------------------
+  
+  ALLOCATE (points(nip,ndim),coord(nod,ndim),jac(ndim,ndim),kc(ntot,ntot),    &
+            der(ndim,nod),deriv(ndim,nod),kcx(ntot,ntot),weights(nip),        &
+            g(ntot),pmul_pp(ntot,nels_pp),utemp_pp(ntot,nels_pp),col(ntot,1), &
+            num(nod),g_g_pp(ntot,nels_pp),no(1),kcy(ntot,ntot),val(1),        &
+            no_local_temp(1),row(1,ntot),diag_precon_tmp(ntot,nels_pp),       &
+            val_f(1),eld(ntot),no_f(1),no_local_temp_f(1),kcz(ntot,ntot))
+
+  IF(numpe==1) PRINT *, " *** Allocated dynamic arrays in: ",                 &
+                          elap_time()-timest(6)," s"
+
+
+  CALL box_bc8(rest,nxe,nye,nze)
+  ielpe = iel_start
+  nres  = nxe*(nze-1)+1
+  IF(loaded_freedoms>0) THEN ; no  = nres ;   val = 10.  ; END IF
+  IF(fixed_freedoms>0)  THEN ; no_f= nres ; val_f = 100. ; END IF 
+
+  CALL sample(element,points,weights)
+
+!------------------------------------------------------------------------------
+! 5. Loop the elements to find the steering array and the number of equations
+!    to solve.
+!------------------------------------------------------------------------------
+      
+  CALL rearrange_2(rest)  
+
+  g_g_pp = 0
+
+! elements_1: DO iel = 1, nels_pp
+!   CALL find_g4(g_num_pp(:,iel),g_g_pp(:,iel),rest)
+! END DO elements_1
+!   
+! neq = 0
+
+!-- remove
+
+  elements_0: DO iel = 1 , nels_pp
+    CALL geometry_8bxz(ielpe,nxe,nze,aa,bb,cc,coord,num)
+    CALL find_g4(num,g,rest) ; g_num_pp(:,iel) = num
+    p_g_co_pp(:,:,iel) = coord; g_g_pp(:,iel) = g; ielpe = ielpe+1 
+    i = MAXVAL(g); j = MAXVAL(num)
+    IF(i>neq_temp)neq_temp = i; IF(j>nn_temp)nn_temp = j
+  END DO elements_0
+  neq = reduce(neq_temp)  ;  nn = reduce(nn_temp)
+
+!-- remove
+   
+! elements_2: DO iel = 1, nels_pp
+!   i = MAXVAL(g_g_pp(:,iel))
+!   IF(i > neq) neq = i
+! END DO elements_2
+!
+! neq = MAX_INTEGER_P(neq)
+
+  timest(7) = elap_time()
+
+!------------------------------------------------------------------------------
+! 6. Create interprocessor communication tables
+!------------------------------------------------------------------------------
+
+  CALL calc_neq_pp          
+  CALL calc_npes_pp(npes,npes_pp)
+  CALL make_ggl2(npes_pp,npes,g_g_pp)
+
+  timest(8) = elap_time()
+
+  IF(numpe==1) PRINT *, " *** Created ggl in: ", timest(8)-timest(7), " s"
+
+!------------------------------------------------------------------------------
+! 7. Allocate arrays dimensioned by neq_pp 
+!------------------------------------------------------------------------------
+
+  ALLOCATE(p_pp(neq_pp),r_pp(neq_pp),x_pp(neq_pp),xnew_pp(neq_pp),            &
+           u_pp(neq_pp),diag_precon_pp(neq_pp),d_pp(neq_pp),store_pp(neq_pp))
+
+  r_pp           = zero ; p_pp     = zero ; x_pp = zero ; xnew_pp = zero
+  diag_precon_pp = zero ; store_pp = zero
+
+  timest(9) = elap_time()
+
+  IF(numpe==1) PRINT *, " *** Allocated arrays dimensioned by neq_pp in: ",   &
+                          timest(9)-timest(8), " s"
+
+!------------------------------------------------------------------------------
+! 8. Element stiffness integration and storage
+!------------------------------------------------------------------------------
+
+!-- remove
+
+   diag_precon_tmp = zero
+
    DO i=1,neq_pp; IF(nres==ieq_start+i-1) THEN; it = numpe;is = i; END IF
    END DO
-   ALLOCATE(p_pp(neq_pp),r_pp(neq_pp),x_pp(neq_pp),xnew_pp(neq_pp),         &
-            u_pp(neq_pp),diag_precon_pp(neq_pp),d_pp(neq_pp),store_pp(neq_pp))
-            r_pp = .0_iwp; p_pp = .0_iwp; x_pp = .0_iwp; xnew_pp = .0_iwp
-            diag_precon_pp = .0_iwp ;   store_pp = .0_iwp
-!---------- element stiffness integration and build the preconditioner---------
-      iel=1;CALL geometry_8bxz(iel,nxe,nze,aa,bb,cc,coord,num)
-      kcx = .0_iwp; kcy = .0_iwp; kcz = .0_iwp
-      gauss_pts_1:  DO i=1,nip
-                CALL shape_der (der,points,i) ; jac = MATMUL(der,coord)
-                det=determinant(jac);CALL invert(jac);deriv = MATMUL(jac,der)
-                row(1,:) = deriv(1,:); eld=deriv(1,:); col(:,1) = eld
-                kcx = kcx + MATMUL(col,row)*det*weights(i)
-                row(1,:) = deriv(2,:); eld=deriv(2,:); col(:,1) = eld
-                kcy = kcy + MATMUL(col,row)*det*weights(i)
-                row(1,:) = deriv(3,:); eld=deriv(3,:); col(:,1) = eld
-                kcz = kcz + MATMUL(col,row)*det*weights(i)
-      END DO gauss_pts_1        ; kc = kcx*kx + kcy*ky + kcz*kz 
-     elements_1: DO iel = 1,nels_pp  
-       DO k=1,ntot;diag_precon_tmp(k,iel)=diag_precon_tmp(k,iel)+kc(k,k);END DO    
-     END DO elements_1
-     CALL scatter(diag_precon_pp,diag_precon_tmp);DEALLOCATE(diag_precon_tmp)
+
+   iel=1;CALL geometry_8bxz(iel,nxe,nze,aa,bb,cc,coord,num)
+   kcx = .0_iwp; kcy = .0_iwp; kcz = .0_iwp
+   gauss_pts_1:  DO i=1,nip
+     CALL shape_der (der,points,i) ; jac = MATMUL(der,coord)
+     det=determinant(jac);CALL invert(jac);deriv = MATMUL(jac,der)
+     row(1,:) = deriv(1,:); eld=deriv(1,:); col(:,1) = eld
+     kcx = kcx + MATMUL(col,row)*det*weights(i)
+     row(1,:) = deriv(2,:); eld=deriv(2,:); col(:,1) = eld
+     kcy = kcy + MATMUL(col,row)*det*weights(i)
+     row(1,:) = deriv(3,:); eld=deriv(3,:); col(:,1) = eld
+     kcz = kcz + MATMUL(col,row)*det*weights(i)
+    END DO gauss_pts_1        ; kc = kcx*kx + kcy*ky + kcz*kz 
+    elements_1: DO iel = 1,nels_pp  
+    DO k=1,ntot;diag_precon_tmp(k,iel)=diag_precon_tmp(k,iel)+kc(k,k);END DO    
+    END DO elements_1
+    CALL scatter(diag_precon_pp,diag_precon_tmp);DEALLOCATE(diag_precon_tmp)
      IF(numpe==it)THEN
       OPEN (11,FILE='p123.res',STATUS='REPLACE',ACTION='WRITE')              
       WRITE(11,'(A,I5,A)') "This job ran on ", npes , "  processors" 
@@ -116,6 +245,145 @@ PROGRAM p123
                            neq," equations"
       WRITE(11,*) "Time after setup is   :", elap_time( ) - timest(1)
      END IF
+
+!-- end remove
+ 
+! CALL sample(element,points,weights)
+   
+! kcx = zero; kcy = zero; kcz = zero
+
+! elements_3: DO i=1,nels_pp
+!   gauss_pts_1:  DO i=1,nip
+!     CALL shape_der (der,points,i)
+!     jac      = MATMUL(der,g_coord_pp(:,:,iel))
+!     det      = determinant(jac)
+!     CALL invert(jac)
+!     deriv    = MATMUL(jac,der)
+!     row(1,:) = deriv(1,:)
+!     eld      = deriv(1,:)
+!     col(:,1) = eld
+!     kcx      = kcx + MATMUL(col,row)*det*weights(i)
+!     row(1,:) = deriv(2,:)
+!     eld      = deriv(2,:)
+!     col(:,1) = eld
+!     kcy      = kcy + MATMUL(col,row)*det*weights(i)
+!     row(1,:) = deriv(3,:)
+!     eld      = deriv(3,:)
+!     col(:,1) = eld
+!     kcz      = kcz + MATMUL(col,row)*det*weights(i)
+!   END DO gauss_pts_1        
+!
+!   kc = kcx*kx + kcy*ky + kcz*kz 
+!    
+!   storkc_pp here?
+!
+!   timest(10) = elap_time()
+!
+!   IF(numpe==1) PRINT *, " *** Computed element stiffness matrices in: ",    &
+!                           timest(10)-timest(9), " s"
+!
+!------------------------------------------------------------------------------
+! 9. Build the diagonal preconditioner
+!------------------------------------------------------------------------------
+
+! ALLOCATE(diag_precon_tmp(ntot,nels_pp))
+! diag_precon_tmp = zero
+!
+! elements_4: DO iel = 1,nels_pp
+!   DO i = 1,ndof
+!     diag_precon_tmp(i,iel) = diag_precon_tmp(i,iel) + storkc_pp(i,i,iel)
+!   END DO
+! END DO elements_4
+!
+! CALL scatter(diag_precon_pp,diag_precon_tmp)
+!
+! DEALLOCATE(diag_precon_tmp)
+!
+! timest(11) = elap_time()
+!
+! IF(numpe==1) PRINT *, " *** Built diagonal preconditioner in: ",            &
+!                         timest(11)-timest(10), " s"
+!  
+!------------------------------------------------------------------------------
+! 10. Read in fixed nodal displacements and assign to equations
+!------------------------------------------------------------------------------
+!
+! IF(fixed_freedoms > 0) THEN
+!
+!   ALLOCATE(node(fixed_freedoms),no(fixed_freedoms),valf(fixed_freedoms),    &
+!            no_pp_temp(fixed_freedoms),sense(fixed_freedoms),                &
+!            no_global(fixed_freedoms))
+!
+!   node = 0 ; no = 0 ; no_pp_temp = 0 ; sense = 0 ; no_global = 0
+!   valf = zero
+!
+!   CALL read_fixed(job_name,numpe,node,sense,valf)
+!   find_no2(g_g_pp,g_num_pp,node,sense,fixed_freedoms_pp,                    &
+!            fixed_freedoms_start,no)
+!   CALL MPI_ALLREDUCE(no,no_global,fixed_freedoms,MPI_INTEGER,MPI_MAX,       &
+!                      MPI_COMM_WORLD,ier)
+!   CALL reindex_fixed_nodes(ieq_start,no_global,no_pp_temp,                  &
+!                            fixed_freedoms_pp,fixed_freedoms_start,neq_pp)
+!
+!   ALLOCATE(no_pp(fixed_freedoms_pp),store_pp(fixed_freedoms_pp))
+!
+!   no_pp    = 0
+!   store_pp = zero
+!   no_pp    = no_pp_temp(1:fixed_freedoms_pp)
+!
+!   DEALLOCATE(node,no,sense,no_pp_temp)
+!
+! END IF
+!
+! IF(fixed_freedoms == 0) fixed_freedoms_pp = 0
+!
+! DEALLOCATE(rest)
+!
+!------------------------------------------------------------------------------
+! 11. Read in loaded nodes and get starting r_pp
+!------------------------------------------------------------------------------
+!
+! IF(loaded_nodes > 0) THEN
+!
+!   ALLOCATE(node(loaded_nodes),val(ndim,loaded_nodes))
+!
+!   val = zero ; node = 0
+!
+!   CALL read_loads(job_name,numpe,node,val)
+!   CALL load(g_g_pp,g_num_pp,node,val,r_pp(1:))
+!
+!   tload = SUM_P(r_pp(1:))
+!
+!   DEALLOCATE(node,val)
+!
+! ELSE
+!
+!   tload = zero
+!
+! END IF
+!
+! DEALLOCATE(g_g_pp)
+!
+! timest(12) = elap_time()
+!
+! IF(numpe==1) PRINT *, " *** Applied loads in:",                             &
+!                         timest(12)-timest(11), " s"
+!
+!------------------------------------------------------------------------------
+! 12. Invert the preconditioner. 
+!     If there are fixed freedoms, first apply a penalty
+!------------------------------------------------------------------------------
+!
+! IF(fixed_freedoms_pp > 0) THEN
+!   DO i = 1,fixed_freedoms_pp
+!      j =  no_pp(i) - ieq_start + 1
+!      diag_precon_pp(j) = diag_precon_pp(j) + penalty
+!      store_pp(i)       = diag_precon_pp(j)
+!   END DO
+! END IF
+!
+! diag_precon_pp = 1._iwp/diag_precon_pp
+!
 !-------------------- get starting r-----------------------------------------
     IF(loaded_freedoms>0) THEN
      CALL reindex_fixed_nodes(ieq_start,no,no_local_temp,num_no,              &
