95e9cf4477d8971c2aad85cdf63987a96a3cbeb5
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2012-04-26 18:47:46 +0000

    Interim save of p124.f90. Not working yet.

--- trunk/parafem/src/programs/4th_ed/p124/p124.f90
+++ trunk/parafem/src/programs/4th_ed/p124/p124.f90
@@ -1,136 +1,218 @@
   PROGRAM p124      
-!-------------------------------------------------------------------------
-!      Program 8.3 conduction equation on 3-d box shaped 
-!      volume using 8-node hexahedral elements : pcg version implicit
-!      integration in time using 'theta' method : parallel version : box_bc
-!-------------------------------------------------------------------------
- USE new_library
- USE geometry_lib
- USE precision
- USE utility
- USE mp_module
- USE timing
- USE global_variables1
- USE gather_scatter6
+!------------------------------------------------------------------------------
+!      Program 12.4 conduction equation using 8-node hexahedral elements;
+!      pcg version implicit; integration in time using 'theta' method
+!      parallel version
+!------------------------------------------------------------------------------
+
+ USE precision  ; USE global_variables ; USE mp_interface
+ USE input      ; USE output           ; USE loading
+ USE timing     ; USE maths            ; USE gather_scatter
+ USE partition  ; USE elements         ; USE steering
+ USE geometry   ; USE pcg
+ 
  IMPLICIT NONE
-!----- ndof,nels,neq,ntot are now global variables - not declared---------
- INTEGER::nxe,nye,nze,nn,nr,nip,nodof=1,nod=8,ndim=3,neq_temp,nn_temp,i,j,&
-   k,iel,nstep,npri,nres,iters,limit,it,is,nlen
- REAL(iwp)::aa,bb,cc,kx,ky,kz,det,theta,dtim,val0,real_time,tol,alpha,    &
-   beta,up,big
- LOGICAL::converged
- CHARACTER(LEN=15)::element='hexahedron',argv
-!------------------------- dynamic arrays----------------------------------
- REAL(iwp),ALLOCATABLE::loads_pp(:),u_pp(:),p_pp(:),points(:,:),kay(:,:), &
-   coord(:,:),fun(:),jac(:,:),der(:,:),deriv(:,:),weights(:),d_pp(:),     &
-   kc(:,:),pm(:,:),funny(:,:),p_g_co_pp(:,:,:),storka_pp(:,:,:),          &
-   storkb_pp(:,:,:),x_pp(:),xnew_pp(:),pmul_pp(:,:),utemp_pp(:,:),        &
-   diag_precon_pp(:),diag_precon_tmp(:,:)
- INTEGER,ALLOCATABLE::rest(:,:),g(:),num(:),g_num_pp(:,:),g_g_pp(:,:)
-!----------------------input and initialisation---------------------------
- timest(1)=elap_time()
- CALL find_pe_procs(numpe,npes)
- IF(numpe==npes)THEN
-   CALL getname(argv,nlen)
-   OPEN(10,FILE=argv(1:nlen)//'.dat',STATUS='OLD',ACTION='READ')
-   READ(10,*)nels,nxe,nze,nip,aa,bb,cc,kx,ky,kz,dtim,nstep,theta,npri,tol,&
-     limit,val0
- END IF
- CALL bcast_inputdata_p124(numpe,npes,nels,nxe,nze,nip,aa,bb,cc,kx,ky,kz, &
-   dtim,nstep,theta,npri,tol,limit,val0)
- CALL calc_nels_pp
- ndof=nod*nodof
- ntot=ndof
- nn_temp=0
- neq_temp=0
- nye=nels/nxe/nze
- nr=(nxe+1)*(nye+1)+(nxe+1)*nze+nye*nze
- ielpe=iel_start
- ALLOCATE(rest(nr,nodof+1),points(nip,ndim),weights(nip),kay(ndim,ndim),  &
-   coord(nod,ndim),fun(nod),jac(ndim,ndim),der(ndim,nod),g(ntot),         &
-   p_g_co_pp(nod,ndim,nels_pp),deriv(ndim,nod),pm(ntot,ntot),             &
-   g_num_pp(nod,nels_pp),kc(ntot,ntot),funny(1,nod),num(nod),             &
-   g_g_pp(ntot,nels_pp),storka_pp(ntot,ntot,nels_pp),                     &
-   utemp_pp(ntot,nels_pp),storkb_pp(ntot,ntot,nels_pp),                   &
-   pmul_pp(ntot,nels_pp),diag_precon_tmp(ntot,nels_pp))
- kay=0.0_iwp
- kay(1,1)=kx
- kay(2,2)=ky
- kay(3,3)=kz
+
+!------------------------------------------------------------------------------
+! 1. Declare variables used in the main program
+!------------------------------------------------------------------------------
+
+! neq,ntot are now global variables - not declared
+
+ INTEGER, PARAMETER :: nodof=1,nod=8,ndim=3
+ INTEGER            :: nxe,nye,nze,nn,nr,nip,neq_temp,nn_temp,i,j
+ INTEGER            :: k,iel,nstep,npri,nres,iters,limit,it,is,nlen
+ REAL(iwp)          :: aa,bb,cc,kx,ky,kz,det,theta,dtim,val0,real_time
+ REAL(iwp)          :: tol,alpha,beta,up,big
+ LOGICAL            :: converged = .false.
+ CHARACTER(LEN=15)  :: element='hexahedron'
+ CHARACTER(LEN=50)  :: fname,job_name,label
+ CHARACTER(LEN=50)  :: program_name='p124'
+
+!------------------------------------------------------------------------------
+! 2. Declare dynamic arrays
+!------------------------------------------------------------------------------ 
+
+ REAL(iwp),ALLOCATABLE :: loads_pp(:),u_pp(:),p_pp(:),points(:,:),kay(:,:)
+ REAL(iwp),ALLOCATABLE :: coord(:,:),fun(:),jac(:,:),der(:,:),deriv(:,:)
+ REAL(iwp),ALLOCATABLE :: weights(:),d_pp(:),kc(:,:),pm(:,:),funny(:,:)
+ REAL(iwp),ALLOCATABLE :: p_g_co_pp(:,:,:),storka_pp(:,:,:)
+ REAL(iwp),ALLOCATABLE :: storkb_pp(:,:,:),x_pp(:),xnew_pp(:),pmul_pp(:,:)
+ REAL(iwp),ALLOCATABLE :: utemp_pp(:,:),diag_precon_pp(:),diag_precon_tmp(:,:)
+ INTEGER,ALLOCATABLE   :: rest(:,:),g(:),num(:),g_num_pp(:,:),g_g_pp(:,:)
+ 
+!------------------------------------------------------------------------------
+! 3. Read job_name from the command line. 
+!    Read control data, mesh data, boundary and loading conditions
+!------------------------------------------------------------------------------ 
+
+  ALLOCATE(timest(25))
+  timest    = zero
+  timest(1) = elap_time()
+
+  CALL find_pe_procs(numpe,npes)
+
+  argc = iargc()
+  IF(argc /= 1) CALL job_name_error(numpe,program_name)
+  CALL GETARG(1,job_name)
+
+  CALL read_p124(job_name,numpe,dtim,element,fixed_freedoms,kx,ky,kz,limit,        &
+                 loaded_nodes,meshgen,nels,nip,nn,nod,npri,nr,nstep,partitioner,   &
+                 theta,tol,val0)
+
+  ! read_p124 needs to be written
+  
+  CALL calc_nels_pp(job_name,nels,npes,numpe,partitioner,nels_pp)
+
+  ndof = nod*nodof
+  ntot = ndof
+
+  ALLOCATE(g_num_pp(nod,nels_pp))
+  ALLOCATE(g_coord_pp(nod,ndim,nels_pp))
+  ALLOCATE(rest(nr,nodof+1))
+
+  g_num_pp   = 0
+  g_coord_pp = zero
+  rest       = 0
+
+  timest(2) = elap_time()
+
+  CALL read_g_num_pp2(job_name,iel_start,nn,npes,numpe,g_num_pp)
+  timest(3) = elap_time()
+
+  IF(meshgen == 2) CALL abaqus2sg(element,g_num_pp)
+  timest(4) = elap_time()
+
+  CALL read_g_coord_pp(job_name,g_num_pp,nn,npes,numpe,g_coord_pp)
+  timest(5) = elap_time()
+
+  CALL read_rest(job_name,numpe,rest)
+  timest(6) = elap_time()
+
+  IF(numpe==1) PRINT *, " *** Read input data in: ", timest(6)-timest(1)," s"
+  
+! nn_temp=0
+! neq_temp=0
+! nye=nels/nxe/nze
+! nr=(nxe+1)*(nye+1)+(nxe+1)*nze+nye*nze
+! ielpe=iel_start
+
+!------------------------------------------------------------------------------
+! 4. Allocate dynamic arrays used in main program
+!------------------------------------------------------------------------------
+
+ ALLOCATE (rest(nr,nodof+1),points(nip,ndim),weights(nip),kay(ndim,ndim),     &
+           coord(nod,ndim),fun(nod),jac(ndim,ndim),der(ndim,nod),g(ntot),     &
+           p_g_co_pp(nod,ndim,nels_pp),deriv(ndim,nod),pm(ntot,ntot),         &
+           g_num_pp(nod,nels_pp),kc(ntot,ntot),funny(1,nod),num(nod),         &
+           g_g_pp(ntot,nels_pp),storka_pp(ntot,ntot,nels_pp),                 &
+           utemp_pp(ntot,nels_pp),storkb_pp(ntot,ntot,nels_pp),               &
+           pmul_pp(ntot,nels_pp),diag_precon_tmp(ntot,nels_pp))
+
+  IF(numpe==1) PRINT *, " *** Allocated dynamic arrays in: ",                 &
+                          elap_time()-timest(6)," s"
+
+!------------------------------------------------------------------------------
+! 5. Loop the elements to find the steering array and the number of equations
+!    to solve.
+!------------------------------------------------------------------------------
+
+  CALL rearrange_2(rest)  
+
+  g_g_pp = 0
+
+  elements_1: DO iel = 1, nels_pp
+    CALL find_g4(g_num_pp(:,iel),g_g_pp(:,iel),rest)
+  END DO elements_1
+   
+  neq = 0
+
+  elements_2: DO iel = 1, nels_pp
+    i = MAXVAL(g_g_pp(:,iel))
+    IF(i > neq) neq = i
+  END DO elements_2
+
+  neq = MAX_INTEGER_P(neq)
+
+  timest(7) = elap_time()
+
+!------------------------------------------------------------------------------
+! 6. Create interprocessor communication tables
+!------------------------------------------------------------------------------
+
+  CALL calc_neq_pp          
+  CALL calc_npes_pp(npes,npes_pp)
+  CALL make_ggl2(npes_pp,npes,g_g_pp)
+
+  timest(8) = elap_time()
+
+  IF(numpe==1) PRINT *, " *** Created ggl in: ", timest(8)-timest(7), " s"
+
+!------------------------------------------------------------------------------
+! 7. Allocate arrays dimensioned by neq_pp 
+!------------------------------------------------------------------------------
+
+  ALLOCATE(loads_pp(neq_pp),diag_precon_pp(neq_pp),u_pp(neq_pp),              &
+           d_pp(neq_pp),p_pp(neq_pp),x_pp(neq_pp),xnew_pp(neq_pp))
+           
+
+  loads_pp  = zero ; diag_precon_pp = zero ; u_pp = zero
+  d_pp      = zero ; p_pp           = zero ; x_pp = zero ; xnew_pp = zero
+
+  timest(9) = elap_time()
+
+  IF(numpe==1) PRINT *, " *** Allocated arrays dimensioned by neq_pp in: ",   &
+                          timest(9)-timest(8), " s"  
+
+  
+! kay=0.0_iwp
+! kay(1,1)=kx
+! kay(2,2)=ky
+! kay(3,3)=kz
+! CALL box_bc8(nxe,nye,nze,rest)
+
+!------------------------------------------------------------------------------
+! 8. Element stiffness integration and storage
+!------------------------------------------------------------------------------
+
  CALL sample(element,points,weights)
- CALL box_bc8(nxe,nye,nze,rest)
- CALL rearrange_2(rest)
-!-------------loop the elements to  set up global arrays -----------------
- elements_1: DO iel=1,nels_pp
-   CALL geometry_8bxz(ielpe,nxe,nze,aa,bb,cc,coord,num)
-   CALL find_g4(num,g,rest)
-   g_num_pp(:,iel)=num
-   p_g_co_pp(:,:,iel)=coord
-   g_g_pp(:,iel)=g
-   ielpe=ielpe+1
-   i=MAXVAL(g)
-   j=MAXVAL(num)
-   IF(i>neq_temp)neq_temp=i
-   IF(j>nn_temp)nn_temp=j
- END DO elements_1
- neq=reduce(neq_temp)
- nn=reduce(nn_temp)
- nres=nxe*(nze-1)+1
- CALL calc_neq_pp
- CALL make_ggl(g_g_pp)
- DO i=1,neq_pp
-   IF(nres==ieq_start+i-1)THEN
-     it=numpe
-     is=i
-   END IF
- END DO
- IF(numpe==it)THEN
-   OPEN(11,FILE=argv(1:nlen)//'.res',STATUS='REPLACE',ACTION='WRITE')              
-   WRITE(11,'(A,I5,A)')"This job ran on  ",npes,"  processors"  
-   WRITE(11,'(A)')"Global coordinates and node numbers"
-   DO i=1,nels_pp,nels_pp - 1                                        
-     WRITE(11,'(A,I8)')"Element ",i
-     num=g_num_pp(:,i)
-     DO k=1,nod
-       WRITE(11,'(A,I8,3E12.4)')"  Node",num(k),p_g_co_pp(k,:,i)
-     END DO
-   END DO
-   WRITE(11,'(A,3(I8,A))')"There are ",nn," nodes",nr," restrained and",  &
-     neq," equations"
-   WRITE(11,*)"Time after setup  is   :",elap_time()-timest(1)
- END IF
- ALLOCATE(loads_pp(neq_pp),diag_precon_pp(neq_pp),u_pp(neq_pp),           &
-   d_pp(neq_pp),p_pp(neq_pp),x_pp(neq_pp),xnew_pp(neq_pp))
- storka_pp=.0_iwp
- storkb_pp=.0_iwp
- diag_precon_tmp=.0_iwp
- p_pp=.0_iwp
- diag_precon_pp=.0_iwp
- xnew_pp=.0_iwp
-!----------- element integration ,storage and build preconditioner --------
- elements_2: DO iel=1,nels_pp
-   num=g_num_pp(:,iel)
-   g=g_g_pp(:,iel)
-   coord=p_g_co_pp(:,:,iel)
-   kc=0.0_iwp
-   pm=0.0_iwp
+
+ storka_pp = zero 
+ storkb_pp = zero
+ 
+ elements_3: DO iel=1,nels_pp
+
+   kc = zero ; pm = zero
+
    gauss_pts: DO i=1,nip
-     CALL shape_der (der,points,i)
+     CALL shape_der(der,points,i)
      CALL shape_fun(fun,points,i)
-     funny(1,:)=fun(:)
-     jac=MATMUL(der,coord)
-     det=determinant(jac)
+     funny(1,:) = fun(:)
+     jac        = MATMUL(der,g_coord_pp(:,:,iel))
+     det        = determinant(jac)
      CALL invert(jac)
-     deriv=MATMUL(jac,der)
-     kc=kc+MATMUL(MATMUL(TRANSPOSE(deriv),kay),deriv)*det*weights(i)
-     pm=pm+MATMUL(TRANSPOSE(funny),funny)*det*weights(i) 
+     deriv      = MATMUL(jac,der)
+     kc         = kc + MATMUL(MATMUL(TRANSPOSE(deriv),kay),deriv)*det*weights(i)
+     pm         = pm + MATMUL(TRANSPOSE(funny),funny)*det*weights(i) 
    END DO gauss_pts
+   
    storka_pp(:,:,iel)=pm+kc*theta*dtim
    storkb_pp(:,:,iel)=pm-kc*(1._iwp-theta)*dtim
-   DO k=1,ntot
-     diag_precon_tmp(k,iel)=diag_precon_tmp(k,iel)+storka_pp(k,k,iel)
-   END DO
- END DO elements_2
+   
+ END DO elements_3
+ 
+!------------------------------------------------------------------------------
+! 9. Build the diagonal preconditioner
+!------------------------------------------------------------------------------ 
+  
+  ALLOCATE(diag_precon_tmp(ntot,nels_pp))
+  diag_precon_tmp = zero
+ 
+    DO k=1,ntot
+      diag_precon_tmp(k,iel)=diag_precon_tmp(k,iel)+storka_pp(k,k,iel)
+    END DO
+ 
+ 
  CALL scatter(diag_precon_pp,diag_precon_tmp)
  DEALLOCATE(diag_precon_tmp)
  diag_precon_pp=1._iwp/diag_precon_pp 
@@ -180,6 +262,34 @@
    IF(j/npri*npri==j.AND.numpe==it)WRITE(11,'(2E12.4,I5)')real_time,      &
      loads_pp(is),iters
  END DO timesteps
+
+! Smith and Griffiths output
+
+ DO i=1,neq_pp
+   IF(nres==ieq_start+i-1)THEN
+     it=numpe
+     is=i
+   END IF
+ END DO
+
+ IF(numpe==it)THEN
+   OPEN(11,FILE=argv(1:nlen)//'.res',STATUS='REPLACE',ACTION='WRITE')              
+   WRITE(11,'(A,I5,A)')"This job ran on  ",npes,"  processors"  
+   WRITE(11,'(A)')"Global coordinates and node numbers"
+   DO i=1,nels_pp,nels_pp - 1                                        
+     WRITE(11,'(A,I8)')"Element ",i
+     num=g_num_pp(:,i)
+     DO k=1,nod
+       WRITE(11,'(A,I8,3E12.4)')"  Node",num(k),p_g_co_pp(k,:,i)
+     END DO
+   END DO
+   WRITE(11,'(A,3(I8,A))')"There are ",nn," nodes",nr," restrained and",  &
+     neq," equations"
+   WRITE(11,*)"Time after setup  is   :",elap_time()-timest(1)
+ END IF
+
  IF(numpe==it)WRITE(11,*)"This analysis took  :",elap_time()-timest(1)
+
  CALL shutdown() 
+
 END PROGRAM p124
