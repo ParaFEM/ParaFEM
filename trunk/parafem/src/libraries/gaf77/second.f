fa63e752dd989c3f39a4214b5662d1366bfd55ef
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2012-05-29 10:48:13 +0000

    Added sim3de.f to gaf77 library Modified Makefile and rfem.f90 Modified second.f to remove compile error but not tested

--- trunk/parafem/src/libraries/gaf77/second.f
+++ trunk/parafem/src/libraries/gaf77/second.f
@@ -12,11 +12,16 @@ c  Returns elapsed user execution time of the calling process in seconds.
 c  This routine tends to be sysem specific and you may need to customize it
 c  for your environment. This routine works for Sun's and VaX's running
 c  Ultrix f77.
+c
+c  28 May 2012 - Replaced etime with cpu_time. Bug fix for 
+c                Cray fortran
 c---------------------------------------------------------------------------
       real function second()
-      real tyme(2)
+      real tyme
+      intrinsic cpu_time
 
-      second = etime(tyme)
+      call cpu_time(tyme)
+      second = tyme
 C                               and for the HP-9000
 c     second = 1.e-6*float(clock())
 
