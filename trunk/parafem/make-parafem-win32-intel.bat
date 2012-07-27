05d140e3424898ea15fd611a621b212f72571ebd
louise.m.lever@gmail.com <louise.m.lever@gmail.com@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2012-07-27 13:31:43 +0000

    Updated ifort make

--- trunk/parafem/make-parafem-win32-intel.bat
+++ trunk/parafem/make-parafem-win32-intel.bat
@@ -2,10 +2,10 @@ echo
 rem  *** EDIT THE NEXT THREE LINES IF NECESSARY ***
 rem
 rem
-set PARAFEM=c:\parafem\parafem
+set PARAFEM=D:\dev\parafem\parafem
 rem !set G95=c:\MinGW\g95\bin\g95
 set G95=ifort /integer-size:32 /real-size:64 /Od
-set FFLAGS=-c 
+set FFLAGS=-c -I%PARAFEM%\include
 set AR=lib /out:
 set VER=1
 rem
@@ -25,7 +25,8 @@ rem
 cd /D %PARAFEM%\src\libraries\dummy_mpi
 del *.lib *.obj *.mod
 %G95% %FFLAGS% mpi_stubs.f90
-%AR%libmpi_stubs.lib mpi_stubs.obj 
+%AR%libmpi_stubs.lib mpi_stubs.obj
+move mpi_stubs.mod %PARAFEM%\include
 move libmpi_stubs.lib %PARAFEM%\lib
 del *.obj
 rem
@@ -59,19 +60,30 @@ cd /D %PARAFEM%\src\modules\shared
 move *.obj %PARAFEM%\src\modules\mpi
 move *.mod %PARAFEM%\src\modules\mpi
 cd /D %PARAFEM%\src\modules\mpi
-%G95% %FFLAGS%  mp_interface.f90
+%G95% %FFLAGS%  mpi_wrapper.f90
+%G95% %FFLAGS%  mp_interface.f90 -I../../libraries/dummy_mpi/include
 %G95% %FFLAGS%  maths.f90
 %G95% %FFLAGS%  input.f90
 %G95% %FFLAGS%  output.f90
 %G95% %FFLAGS%  gather_scatter.f90
 %G95% %FFLAGS%  loading.f90
-%G95% %FFLAGS%  large_strain.f90
+rem %G95% %FFLAGS%  large_strain.f90
 %G95% %FFLAGS%  pcg.f90
 %G95% %FFLAGS%  bicg.f90
 %AR%libparafem.lib *.obj
 move libparafem.lib %PARAFEM%\lib
 move *.mod %PARAFEM%\include
 del *.obj 
+rem
+rem
+rem *** BUILD RFEMCUBE ***
+rem
+rem
+cd /D %PARAFEM%\src\tools\preprocessing\rfemcube
+del rfemcube.exe rfemcube.o
+%G95% rfemcube.f90 %PARAFEM%\lib\libparafem.lib -I%PARAFEM%\include -o rfemcube.exe
+del *.obj *.lib
+move rfemcube.exe %PARAFEM%\bin
 rem
 rem
 rem *** BUILD RFEMBC ***
@@ -93,6 +105,16 @@ del rfemfield.exe rfemfield.obj
 del *.obj *.lib
 move rfemfield.exe %PARAFEM%\bin
 rem
+rem 
+rem *** BUILD RFEMREDUCE ***
+rem
+rem
+cd /D %PARAFEM%\src\tools\preprocessing\rfemreduce
+del rfemreduce.exe rfemreduce.obj
+%G95% rfemreduce.f90 -I%PARAFEM%\include -o rfemreduce.exe
+del *.obj *.lib
+move rfemreduce.exe %PARAFEM%\bin
+rem
 rem *** BUILD RFEMSOLVE ***
 rem
 cd /D %PARAFEM%\src\programs\rfem
@@ -101,3 +123,4 @@ del *.obj *.lib
 move rfemsolve.exe %PARAFEM%\bin
 cd /D %PARAFEM%
 
+:end
