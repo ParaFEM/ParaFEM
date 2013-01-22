239343a216f8eaa6ed7c0d14ade8a019dc3e8e29
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2013-01-22 15:37:57 +0000

    Minor modifications to p125_5.f90

--- trunk/parafem/src/programs/5th_ed/p125_5/p125_5.f90
+++ trunk/parafem/src/programs/5th_ed/p125_5/p125_5.f90
@@ -4,9 +4,10 @@ PROGRAM p125
 !   hexahedral elements and a simple explicit algorithm : parallel version
 !   write on processor it at freedom nres
 !-------------------------------------------------------------------------
+!USE mpi_wrapper !remove comment for serial compilation
  USE precision; USE global_variables; USE mp_interface; USE input
  USE output; USE loading; USE timing; USE maths; USE gather_scatter
- USE partition; USE elements; USE steering; USE geometry; USE pcg
+ USE elements; USE steering; USE geometry
  IMPLICIT NONE
 ! neq,ntot are now global variables - not declared
  INTEGER,PARAMETER::nodof=1,ndim=3
@@ -43,14 +44,11 @@ PROGRAM p125
    pmul_pp(ntot,nels_pp),utemp_pp(ntot,nels_pp))
 !----------  find the steering array and equations per process -----------
  CALL rearrange_2(rest); g_g_pp = 0; neq=0
- elements_1: DO iel = 1, nels_pp
+ elements_0: DO iel = 1, nels_pp
    CALL find_g4(g_num_pp(:,iel),g_g_pp(:,iel),rest)
- END DO elements_1
- elements_2: DO iel = 1, nels_pp
-   i=MAXVAL(g_g_pp(:,iel)); IF(i>neq) neq=i
- END DO elements_2
- neq=MAX_INTEGER_P(neq); CALL calc_neq_pp; CALL calc_npes_pp(npes,npes_pp)
- CALL make_ggl2(npes_pp,npes,g_g_pp)
+ END DO elements_0
+ neq=MAXVAL(g_g_pp); neq=MAX_INTEGER_P(neq); CALL calc_neq_pp
+ CALL calc_npes_pp(npes,npes_pp); CALL make_ggl2(npes_pp,npes,g_g_pp)
  DO i=1,neq_pp; IF(nres==ieq_start+i-1)THEN;it=numpe;is=i;END IF;END DO
  IF(numpe==it)THEN
    OPEN(11,FILE=argv(1:nlen)//".res",STATUS='REPLACE',ACTION='WRITE')              
@@ -65,7 +63,7 @@ PROGRAM p125
 !------------- loop the elements for integration and invert mass ---------
  timest(3)=elap_time(); CALL sample(element,points,weights)
  kay=zero; kay(1,1)=kx; kay(2,2)=ky; kay(3,3)=kz
- elements_3: DO iel=1,nels_pp
+ elements_1: DO iel=1,nels_pp
    kc=zero; pm=zero
     gauss_pts: DO i=1,nip
       CALL shape_der(der,points,i); CALL shape_fun(fun,points,i)
@@ -78,7 +76,7 @@ PROGRAM p125
     pm=.0_iwp; DO i=1,ntot; pm(i,i)=mass(i); END DO
     store_pm_pp(:,:,iel)=pm-kc*dtim
     DO i=1,ntot; globma_tmp(i,iel)=globma_tmp(i,iel)+mass(i); END DO
-  END DO elements_3
+  END DO elements_1
   IF(numpe==it) THEN; WRITE(11,'(A,F10.4)')                              &
     "Time for element integration is :",elap_time()-timest(3); END IF
   CALL scatter(globma_pp,globma_tmp); globma_pp = 1._iwp/globma_pp
@@ -89,10 +87,10 @@ PROGRAM p125
    real_time = j*dtim              
 !---------------  go round the elements  ---------------------------------
    utemp_pp=zero; pmul_pp=zero; CALL gather(loads_pp,pmul_pp)
-   elements_4: DO iel=1,nels_pp  
+   elements_2: DO iel=1,nels_pp  
      pm = store_pm_pp(:,:,iel) 
      utemp_pp(:,iel) = utemp_pp(:,iel)+MATMUL(pm,pmul_pp(:,iel)) 
-   END DO elements_4; CALL scatter(newlo_pp,utemp_pp)
+   END DO elements_2; CALL scatter(newlo_pp,utemp_pp)
    loads_pp=newlo_pp*globma_pp; newlo_pp=zero 
    IF(j/npri*npri==j.AND.numpe==it)                                          &
      WRITE(11,'(2E12.4)')real_time,loads_pp(is) 
