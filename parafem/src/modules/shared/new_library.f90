3020dd5915b102db6b39a8b47922766a1778d5b6
lee.margetts <lee.margetts@manchester.ac.uk>
2013-01-23 08:35:43 +0000

    Added an empty module, new_library.f90 for the 5th Edition of Programming the Finite Element Method.

---
+++ parafem/src/modules/shared/new_library.f90
@@ -0,0 +1,31 @@
+MODULE NEW_LIBRARY
+
+  !/****h* /new_library
+  !*  NAME
+  !*    MODULE: new_library
+  !*  SYNOPSIS
+  !*    Usage:      USE new_library
+  !*  FUNCTION
+  !*    Contains subroutines provided in the Smith and Griffiths text book
+  !*    
+  !*    Subroutine             Purpose
+  !*    
+  !*  AUTHORS
+  !*    I.M. Smith
+  !*    D.V. Griffiths
+  !*    L. Margetts
+  !*  COPYRIGHT
+  !*    2004-2013 University of Manchester
+  !******
+  !*  Place remarks that should not be included in the documentation here.
+  !*
+  !*  Module required by the 5th Edition of "Programming the Finite Element
+  !*  Method. Take care when modifying
+  !*/
+  
+  USE precision
+
+  CONTAINS
+
+ 
+END MODULE NEW_LIBRARY
