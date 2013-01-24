c189b184c4d07d3a8b304c0b022a65b4437bca71
lee.margetts <lee.margetts@manchester.ac.uk>
2013-01-24 11:14:48 +0000

    Moved form_s to maths.f90 module

--- parafem/src/modules/shared/new_library.f90
+++ parafem/src/modules/shared/new_library.f90
@@ -24,7 +24,6 @@ MODULE NEW_LIBRARY
   !*/
   
   USE precision
-  USE maths
 
   CONTAINS
 
@@ -1755,82 +1754,5 @@ END SUBROUTINE formm
     RETURN
 
   END SUBROUTINE SHAPE_DERIVATIVES
-  
-!------------------------------------------------------------------------------
-!------------------------------------------------------------------------------
-!------------------------------------------------------------------------------
-  
-    SUBROUTINE FORM_S(GG,ell,kappa,omega,Gamma,s)
-  
-      !/****f* bicg/form_s
-      !*  NAME
-      !*    SUBROUTINE: form_s
-      !*  SYNOPSIS
-      !*    Usage:      CALL form_s(GG,ell,kappa,omega,gamma,s)
-      !*  FUNCTION
-      !*    Forms the s vector in bicgstab(l)
-      !*  INPUTS
-      !*    The following arguments have the INTENT(IN) attribute:
-      !*
-      !*    GG(:,:)                  : Real
-      !*                             : GG Array
-      !*
-      !*    kappa                    : Real
-      !*                             : Variable
-      !*
-      !*    ell                      : Integer
-      !*                             : Variable
-      !*
-      !*    The following arguments have the INTENT(OUT) attribute:
-      !*
-      !*    omega                    : Real
-      !*                             : Variable
-      !*    
-      !*    Gamma(:)                 : Real   
-      !*                             : Vector
-      !*
-      !*    s(:)                     : Real
-      !*                             : Vector
-      !*
-      !*  AUTHOR
-      !*    I.M. Smith
-      !******
-      !*  Place remarks that should not be included in the documentation here.
-      !*
-      !*/
-  
-      REAL(iwp),INTENT(IN)  :: GG(:,:),kappa
-      INTEGER, INTENT(IN)   :: ell
-      REAL(iwp),INTENT(OUT) :: omega,Gamma(:),s(:)
-      REAL(iwp)             :: HH(ell-1,ell-1),Gamma0(ell+1),p(ell-1),q(ell-1),&
-                               Gamma1(ell+1),NGamma0,NGamma1,cosine
    
-      HH            = -GG(2:ell,2:ell)
-   
-      call invert(HH)  
-   
-      p             = matmul(HH,GG(2:ell,1))  
-      q             = matmul(HH,GG(2:ell,ell+1))
-      Gamma0(1)     = 1.0_iwp
-      Gamma0(ell+1) = 0.0_iwp
-      Gamma0(2:ell) = p
-      Gamma1(1)     = 0.0_iwp
-      Gamma1(ell+1) = 1.0_iwp
-      Gamma1(2:ell) = q
-      NGamma0       = dot_product(Gamma0,matmul(GG,Gamma0))
-      NGamma1       = dot_product(Gamma1,matmul(GG,Gamma1))
-      omega         = dot_product(Gamma0,matmul(GG,Gamma1))
-      cosine        = abs(omega)/sqrt(abs(NGamma0*NGamma1))
-      omega         = omega/NGamma1
-   
-      if(cosine<kappa) omega = (kappa/cosine) * omega
-   
-      Gamma         = Gamma0 - omega * Gamma1
-      s(1:ell)      = Gamma(2:ell+1)       
-      s(ell+1)      = 0.0_iwp
-     
-      RETURN
-      
-  END SUBROUTINE FORM_S
- 
 END MODULE NEW_LIBRARY
