7d1d598faca5c99cb544c63217d16e1a5f59b049
lee.margetts <lee.margetts@manchester.ac.uk>
2013-01-31 21:53:40 +0000

    Edited p125_5.f90 in book style and added output routine for ParaView

--- parafem/src/programs/5th_ed/p125_5/p125_5.f90
+++ parafem/src/programs/5th_ed/p125_5/p125_5.f90
@@ -7,13 +7,12 @@ PROGRAM p125
 !USE mpi_wrapper !remove comment for serial compilation
  USE precision; USE global_variables; USE mp_interface; USE input
  USE output; USE loading; USE timing; USE maths; USE gather_scatter
- USE steering; USE geometry; USE new_library
- IMPLICIT NONE
+ USE geometry; USE new_library; IMPLICIT NONE
 ! neq,ntot are now global variables - not declared
  INTEGER,PARAMETER::nodof=1,ndim=3
- INTEGER::nels,ndof,npes_pp,nn,nr,nip,nod,i,j,k,iel,neq_temp,nn_temp,    &
+ INTEGER::nels,ndof,npes_pp,nn,nr,nip,nod,i,j,k,iel,nstep,nlen,          &
    npri,nres,it,is,meshgen,partitioner,loaded_nodes=0,fixed_freedoms,    &
-   nstep,nlen
+   nodes_pp,node_start,node_end
  REAL(iwp),PARAMETER::zero=0.0_iwp
  REAL(iwp)::kx,ky,kz,det,dtim,val0,real_time
  CHARACTER(LEN=15)::element; CHARACTER(LEN=50)::argv
@@ -21,8 +20,9 @@ PROGRAM p125
  REAL(iwp),ALLOCATABLE::loads_pp(:),points(:,:),kay(:,:),jac(:,:),       &
    der(:,:),deriv(:,:),weights(:),kc(:,:),funny(:,:),globma_pp(:),       &
    fun(:),store_pm_pp(:,:,:),newlo_pp(:),mass(:),globma_tmp(:,:),        &
-   pmul_pp(:,:),utemp_pp(:,:),g_coord_pp(:,:,:),timest(:),pm(:,:) 
- INTEGER,ALLOCATABLE::rest(:,:),g(:),num(:),g_num_pp(:,:),g_g_pp(:,:)         
+   pmul_pp(:,:),utemp_pp(:,:),g_coord_pp(:,:,:),timest(:),pm(:,:),       &
+   ptl_pp(:)
+ INTEGER,ALLOCATABLE::rest(:,:),g_num_pp(:,:),g_g_pp(:,:)         
 !-------------------------- input and initialisation ---------------------
  ALLOCATE(timest(25)); timest=zero; timest(1)=elap_time()
  CALL find_pe_procs(numpe,npes); CALL getname(argv,nlen)
@@ -33,13 +33,13 @@ PROGRAM p125
  ndof=nod*nodof; ntot=ndof
  ALLOCATE(g_num_pp(nod,nels_pp),g_coord_pp(nod,ndim,nels_pp),            &
    rest(nr,nodof+1)); g_num_pp=0; g_coord_pp=zero; rest=0
- CALL read_g_num_pp2(argv,iel_start,nn,npes,numpe,g_num_pp)
- IF(meshgen == 2) CALL abaqus2sg(element,g_num_pp)
+ CALL read_g_num_pp(argv,iel_start,nn,npes,numpe,g_num_pp)
+ IF(meshgen==2) CALL abaqus2sg(element,g_num_pp)
  CALL read_g_coord_pp(argv,g_num_pp,nn,npes,numpe,g_coord_pp)
  CALL read_rest(argv,numpe,rest);timest(2)=elap_time()
  ALLOCATE (points(nip,ndim),weights(nip),kay(ndim,ndim),jac(ndim,ndim),  &
-   der(ndim,nod),deriv(ndim,nod),kc(ntot,ntot),g(ntot),funny(1,nod),     &
-   num(nod),g_g_pp(ntot,nels_pp),store_pm_pp(ntot,ntot,nels_pp),         &
+   der(ndim,nod),deriv(ndim,nod),kc(ntot,ntot),funny(1,nod),             &
+   g_g_pp(ntot,nels_pp),store_pm_pp(ntot,ntot,nels_pp),                  &
    mass(ntot),fun(nod),pm(ntot,ntot),globma_tmp(ntot,nels_pp),           &
    pmul_pp(ntot,nels_pp),utemp_pp(ntot,nels_pp))
 !----------  find the steering array and equations per process -----------
@@ -48,7 +48,7 @@ PROGRAM p125
    CALL find_g4(g_num_pp(:,iel),g_g_pp(:,iel),rest)
  END DO elements_0
  neq=MAXVAL(g_g_pp); neq=MAX_INTEGER_P(neq); CALL calc_neq_pp
- CALL calc_npes_pp(npes,npes_pp); CALL make_ggl2(npes_pp,npes,g_g_pp)
+ CALL calc_npes_pp(npes,npes_pp); CALL make_ggl(npes_pp,npes,g_g_pp)
  DO i=1,neq_pp; IF(nres==ieq_start+i-1)THEN;it=numpe;is=i;END IF;END DO
  IF(numpe==it)THEN
    OPEN(11,FILE=argv(1:nlen)//".res",STATUS='REPLACE',ACTION='WRITE')              
@@ -73,7 +73,7 @@ PROGRAM p125
       pm=pm+MATMUL(TRANSPOSE(funny),funny)*det*weights(i) 
     END DO gauss_pts
     DO i=1,ntot; mass(i)=sum(pm(i,:)); END DO
-    pm=.0_iwp; DO i=1,ntot; pm(i,i)=mass(i); END DO
+    pm=zero; DO i=1,ntot; pm(i,i)=mass(i); END DO
     store_pm_pp(:,:,iel)=pm-kc*dtim
     DO i=1,ntot; globma_tmp(i,iel)=globma_tmp(i,iel)+mass(i); END DO
   END DO elements_1
@@ -83,6 +83,8 @@ PROGRAM p125
   loads_pp=val0; DEALLOCATE(globma_tmp); timest(4)=elap_time()
 !------------------------- time stepping recursion -----------------------
  IF(numpe==it)THEN; WRITE(11,'(A)')"    Time     Pressure"; END IF
+ CALL calc_nodes_pp(nn,npes,numpe,node_end,node_start,nodes_pp)
+ ALLOCATE(ptl_pp(nodes_pp*ndim))
  timesteps: DO j=1,nstep
    real_time = j*dtim              
 !---------------  go round the elements  ---------------------------------
@@ -92,11 +94,23 @@ PROGRAM p125
      utemp_pp(:,iel) = utemp_pp(:,iel)+MATMUL(pm,pmul_pp(:,iel)) 
    END DO elements_2; CALL scatter(newlo_pp,utemp_pp)
    loads_pp=newlo_pp*globma_pp; newlo_pp=zero 
-   IF(j/npri*npri==j.AND.numpe==it)                                          &
-     WRITE(11,'(2E12.4)')real_time,loads_pp(is) 
+   IF(j/npri*npri==j.AND.numpe==it)                                      &
+     WRITE(11,'(2E12.4)')real_time,loads_pp(is)
+!------------------- output potentials for ParaView ----------------------
+     IF(numpe==1)THEN; WRITE(ch,'(I6.6)') j
+       OPEN(12,file=argv(1:nlen)//".ensi.NDPTL-"//ch,status='replace',   &
+         action='write')
+       WRITE(12,'(A)')"Alya Ensight Gold --- Scalar per-node variable file"
+       WRITE(12,'(A/A/A)') "part", "    1","coordinates"
+     END IF
+     ptl_pp=zero; utemp_pp=zero; CALL gather(xnew_pp(1:),utemp_pp)
+     CALL scatter_nodes(npes,nn,nels_pp,g_num_pp,nod,ndim,nodes_pp,      &
+       node_start,node_end,utemp_pp,ptl_pp,1)
+     CALL dismsh_ensi_p(12,1,nodes_pp,npes,numpe,1,ptl_pp); CLOSE(12)
  END DO timesteps; timest(5)=elap_time()
  IF(numpe==it) THEN
-   WRITE(11,'(A,F10.4)')"Time stepping recursion took  :",timest(5)-timest(4)
+   WRITE(11,'(A,F10.4)')"Time stepping recursion took  :",               &
+     timest(5)-timest(4)
    WRITE(11,'(A,F10.4)')"This analysis took  :",elap_time()-timest(1)
- END IF; CALL shutdown() 
+ END IF; CALL SHUTDOWN() 
 END PROGRAM p125
