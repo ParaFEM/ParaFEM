e3585ed7a5491cc8500159353808b89827227203
lee.margetts <lee.margetts@manchester.ac.uk>
2013-01-31 09:30:28 +0000

    Minor edits to p122_5.f90 for layout. No coding changes.

--- parafem/src/programs/5th_ed/p122_5/p122_5.f90
+++ parafem/src/programs/5th_ed/p122_5/p122_5.f90
@@ -19,58 +19,54 @@ PROGRAM p122
  REAL(iwp)::e,v,det,phi,c,psi,dt,f,dsbar,dq1,dq2,dq3,lode_theta,presc,   &
    sigm,pi,snph,cons,plastol,cjtol,up,alpha,beta,big
  REAL(iwp),PARAMETER::zero=0.0_iwp,penalty=1.e20_iwp
-!---------------------------- dynamic arrays------------------------------
+!---------------------------- dynamic arrays -----------------------------
  REAL(iwp),ALLOCATABLE::loads_pp(:),points(:,:),bdylds_pp(:),valf(:),    &
    evpt_pp(:,:,:),pmul_pp(:,:),dee(:,:),jac(:,:),weights(:),store_pp(:), &
-   oldis_pp(:),der(:,:),deriv(:,:),bee(:,:),km(:,:),eld(:),eps(:),       &
+   oldis_pp(:),der(:,:),deriv(:,:),bee(:,:),eld(:),eps(:),ld0_pp(:),     &
    sigma(:),bload(:),eload(:),erate(:),p_g_co_pp(:,:,:),evp(:),devp(:),  &
    m1(:,:),m2(:,:),m3(:,:),flow(:,:),storkm_pp(:,:,:),r_pp(:),temp(:),   &
    tensor_pp(:,:,:),stress(:),totd_pp(:),qinc(:),p_pp(:),x_pp(:),        &
    xnew_pp(:),u_pp(:),utemp_pp(:,:),diag_precon_pp(:),d_pp(:),disp_pp(:),&
-   diag_precon_tmp(:,:),timest(:),g_coord_pp(:,:,:),val(:,:),ld0_pp(:)
+   diag_precon_tmp(:,:),timest(:),g_coord_pp(:,:,:),val(:,:)
  INTEGER,ALLOCATABLE::rest(:,:),no(:),g_num_pp(:,:),nodef(:),            &
-   g_g_pp(:,:),node(:),sense(:),no_pp_temp(:),no_global(:),no_pp(:)
-!--------------------------input and initialisation----------------------
- ALLOCATE(timest(20)); timest=zero; timest(1)=elap_time()
- CALL find_pe_procs(numpe,npes); CALL getname(argv,nlen) 
- fixed_freedoms=0; fixed_freedoms_pp=0
- CALL read_p122(argv,numpe,c,cjits,cjtol,cons,e,element,fixed_freedoms,   &
-   loaded_nodes,incs,meshgen,nels,nip,nn,nod,nr,phi,partitioner,plasits,  &
-   plastol,psi,v)
+   g_g_pp(:,:),node(:),sense(:),no_pp_temp(:),no_pp(:)
+!------------------------ input and initialisation -----------------------
+ ALLOCATE(timest(20)); timest=zero; timest(1)=elap_time()  
+ CALL find_pe_procs(numpe,npes);  CALL getname(argv,nlen)
+ CALL read_p122(argv,numpe,c,cjits,cjtol,cons,e,element,fixed_freedoms,  &
+   loaded_nodes,incs,meshgen,nels,nip,nn,nod,nr,phi,partitioner,plasits, &
+   plastol,psi,v); IF(fixed_freedoms==0) fixed_freedoms_pp=0
  CALL calc_nels_pp(argv,nels,npes,numpe,partitioner,nels_pp)
  ndof=nod*nodof; ntot=ndof
- ALLOCATE(g_num_pp(nod, nels_pp),g_coord_pp(nod,ndim,nels_pp),            &
+ ALLOCATE(g_num_pp(nod,nels_pp),g_coord_pp(nod,ndim,nels_pp),            &
    rest(nr,nodof+1)); g_num_pp=0; g_coord_pp=zero; rest=0
  CALL read_g_num_pp(argv,iel_start,nn,npes,numpe,g_num_pp)
  IF(meshgen == 2) CALL abaqus2sg(element,g_num_pp)
  CALL read_g_coord_pp(argv,g_num_pp,nn,npes,numpe,g_coord_pp)
  CALL read_rest(argv,numpe,rest); timest(2)=elap_time()
- ALLOCATE(points(nip,ndim),weights(nip),m1(nst,nst),qinc(incs),           &
-   dee(nst,nst),evpt_pp(nst,nip,nels_pp),m2(nst,nst),m3(nst,nst),         &
-   tensor_pp(nst,nip,nels_pp),g_g_pp(ntot,nels_pp),utemp_pp(ntot,nels_pp),&
-   stress(nst),storkm_pp(ntot,ntot,nels_pp),jac(ndim,ndim),der(ndim,nod), &
-   deriv(ndim,nod),bee(nst,ntot),km(ntot,ntot),eld(ntot),eps(nst),        &
-   sigma(nst),bload(ntot),eload(ntot),erate(nst),evp(nst),devp(nst),      &
-   flow(nst,nst),pmul_pp(ntot,nels_pp))
- CALL read_qinc(argv,numpe,qinc); CALL rearrange(rest)
-!----------  find the steering array and equations per process -----------
- g_g_pp=0; neq=0
+ ALLOCATE(points(nip,ndim),weights(nip),m1(nst,nst),qinc(incs),eps(nst), &
+  dee(nst,nst),evpt_pp(nst,nip,nels_pp),m2(nst,nst),m3(nst,nst),evp(nst),&
+  tensor_pp(nst,nip,nels_pp),g_g_pp(ntot,nels_pp),utemp_pp(ntot,nels_pp),&
+  stress(nst),storkm_pp(ntot,ntot,nels_pp),jac(ndim,ndim),der(ndim,nod), &
+  deriv(ndim,nod),bee(nst,ntot),eld(ntot),pmul_pp(ntot,nels_pp),         &
+  sigma(nst),bload(ntot),eload(ntot),erate(nst),devp(nst),flow(nst,nst))
+!----------- find the steering array and equations per process -----------
+ CALL read_qinc(argv,numpe,qinc); CALL rearrange(rest); g_g_pp=0; neq=0
  elements_1: DO iel=1,nels_pp
    CALL find_g(g_num_pp(:,iel),g_g_pp(:,iel),rest)
- END DO elements_1
- neq=MAXVAL(g_g_pp); neq=max_p(neq); CALL calc_neq_pp
+ END DO elements_1; neq=MAXVAL(g_g_pp); neq=max_p(neq); CALL calc_neq_pp
  CALL calc_npes_pp(npes,npes_pp); CALL make_ggl(npes_pp,npes,g_g_pp)
  IF(numpe==1)THEN
    OPEN(11,FILE=argv(1:nlen)//'.res',STATUS='REPLACE',ACTION='WRITE')              
    WRITE(11,'(A,I5,A)')"This job ran on ", npes,"  processes"
-   WRITE(11,'(A,3(I7,A))')"There are ",nn," nodes",nr,                    &
+   WRITE(11,'(A,3(I7,A))')"There are ",nn," nodes",nr,                   &
      " restrained and   ",neq," equations"
    WRITE(11,*)"Time after setup  is  : ",elap_time()-timest(1)
  END IF 
- ALLOCATE(loads_pp(neq_pp),bdylds_pp(neq_pp),oldis_pp(neq_pp),            &
-   r_pp(neq_pp),totd_pp(neq_pp),p_pp(neq_pp),x_pp(neq_pp),xnew_pp(neq_pp),&     
-   u_pp(neq_pp),diag_precon_pp(neq_pp),d_pp(neq_pp),ld0_pp(neq_pp)) 
- totd_pp=zero; tensor_pp=zero; p_pp=zero; xnew_pp=zero; diag_precon_pp=zero
+ ALLOCATE(loads_pp(neq_pp),bdylds_pp(neq_pp),oldis_pp(neq_pp),           &
+   r_pp(neq_pp),totd_pp(neq_pp),p_pp(neq_pp),x_pp(neq_pp),ld0_pp(neq_pp),&     
+   u_pp(neq_pp),diag_precon_pp(neq_pp),d_pp(neq_pp),xnew_pp(neq_pp)) 
+ totd_pp=zero;tensor_pp=zero; p_pp=zero; xnew_pp=zero; diag_precon_pp=zero
  oldis_pp=zero;ld0_pp=zero; pi=ACOS(-1._iwp); snph=SIN(phi*pi/180._iwp)
  dt=4._iwp*(1._iwp+v)*(1._iwp-2._iwp*v)/(e*(1._iwp-2._iwp*v+snph*snph))
  IF(numpe==1)WRITE(11,'(A,E12.4)')"The critical timestep is   ",dt
@@ -90,47 +86,38 @@ PROGRAM p122
    diag_precon_tmp(i,iel) = diag_precon_tmp(i,iel)+storkm_pp(i,i,iel)
  END DO;  END DO elements_2a
  CALL scatter(diag_precon_pp,diag_precon_tmp); DEALLOCATE(diag_precon_tmp)
-!-------------------- invert preconditioner -----------------------------
+!------------------------- invert preconditioner -------------------------
  IF(loaded_nodes>0) THEN
    ALLOCATE(node(loaded_nodes),val(ndim,loaded_nodes)); val=zero; node=0
    CALL read_loads(argv,numpe,node,val)
    CALL load(g_g_pp,g_num_pp,node,val,ld0_pp(1:))
  END IF
  IF(fixed_freedoms>0) THEN
-   ALLOCATE(nodef(fixed_freedoms),no(fixed_freedoms),                   &
-     no_pp_temp(fixed_freedoms),sense(fixed_freedoms),                  &
-!    valf(fixed_freedoms),no_global(fixed_freedoms))
-     valf(fixed_freedoms))
-!  nodef=0; no=0; no_pp_temp=0; sense=0; no_global=0; valf=zero
+   ALLOCATE(nodef(fixed_freedoms),no_pp_temp(fixed_freedoms),            &
+     no(fixed_freedoms),sense(fixed_freedoms),valf(fixed_freedoms))
    nodef=0; no=0; no_pp_temp=0; sense=0; valf=zero
    CALL read_fixed(argv,numpe,nodef,sense,valf)
    CALL find_no(nodef,rest,sense,no)
-!  CALL MPI_ALLREDUCE(no,no_global,fixed_freedoms,MPI_INTEGER,MPI_MAX,  &
-!    MPI_COMM_WORLD,ier)
-!  CALL reindex(ieq_start,no_global,no_pp_temp,fixed_freedoms_pp,       &
-!    fixed_freedoms_start,neq_pp)
-   CALL reindex(ieq_start,no,no_pp_temp,fixed_freedoms_pp,              &
+   CALL reindex(ieq_start,no,no_pp_temp,fixed_freedoms_pp,               &
      fixed_freedoms_start,neq_pp)
    ALLOCATE(no_pp(fixed_freedoms_pp),store_pp(fixed_freedoms_pp))
    no_pp=0; store_pp=0; no_pp=no_pp_temp(1:fixed_freedoms_pp)
    DEALLOCATE(nodef,no,sense,no_pp_temp) 
  END IF
  IF(fixed_freedoms_pp>0)THEN
-   DO i=1,fixed_freedoms_pp
-     j=no_pp(i)-ieq_start+1
+   DO i=1,fixed_freedoms_pp; j=no_pp(i)-ieq_start+1
      diag_precon_pp(j)=diag_precon_pp(j)+penalty
      store_pp(i)=diag_precon_pp(j)
    END DO
  END IF; diag_precon_pp=1._iwp/diag_precon_pp
  CALL calc_nodes_pp(nn,npes,numpe,node_end,node_start,nodes_pp)
  ALLOCATE(disp_pp(nodes_pp*ndim),temp(nodes_pp)); disp_pp=zero; temp=zero
-!------------------------  load  increment loop --------------------------
+!------------------------- load  increment loop --------------------------
  load_increments: do iy=1,incs
    plasiters=0; bdylds_pp=zero; evpt_pp=zero; cjtot=0
    IF(numpe==1) WRITE(11,'(/,A,I5)')"Load Increment   ",iy
-!--------------------------   plastic iteration loop   -------------------
-   plastic_iterations: DO
-     plasiters=plasiters+1; loads_pp=zero
+!------------------------ plastic iteration loop -------------------------
+   plastic_iterations: DO     ; plasiters=plasiters+1; loads_pp=zero
      IF(plasiters==1) THEN
        IF(fixed_freedoms_pp>0) THEN
          DO i=1,fixed_freedoms_pp; j=no_pp(i)-ieq_start+1
@@ -151,17 +138,13 @@ PROGRAM p122
        END IF
      END IF 
 !------ if x=.0 p and r are just loads but in general p=r=loads-A*x ------
-!-----------------------so form r = A * x --------------------------------
      r_pp=zero; CALL gather(x_pp,pmul_pp)
      elements_2b: DO iel=1,nels_pp
        utemp_pp(:,iel)=MATMUL(storkm_pp(:,:,iel),pmul_pp(:,iel))
      END DO elements_2b; CALL scatter(r_pp,utemp_pp)
-!------------------------now precondition r and p ------------------------
-     r_pp=loads_pp-r_pp; d_pp=diag_precon_pp*r_pp; p_pp=d_pp
-!-------------------   solve the simultaneous equations by pcg -----------
-     cjiters=0
-     conjugate_gradients: DO
-       cjiters=cjiters+1; u_pp=zero; pmul_pp=zero
+     r_pp=loads_pp-r_pp; d_pp=diag_precon_pp*r_pp; p_pp=d_pp; cjiters=0
+!---------------- solve the simultaneous equations by pcg ----------------
+     conjugate_gradients: DO  ; cjiters=cjiters+1; u_pp=zero; pmul_pp=zero
        CALL gather(p_pp,pmul_pp)
        elements_3: DO iel=1,nels_pp
          utemp_pp(:,iel)=MATMUL(storkm_pp(:,:,iel),pmul_pp(:,iel))
@@ -172,7 +155,7 @@ PROGRAM p122
            ELSE; u_pp(j)=zero; END IF
          END DO
        END IF
-!-------------------------------pcg process ------------------------------
+!------------------------------ pcg process ------------------------------
        up=DOT_PRODUCT_P(r_pp,d_pp); alpha=up/DOT_PRODUCT_P(p_pp,u_pp)
        xnew_pp=x_pp+p_pp*alpha; r_pp=r_pp-u_pp*alpha
        d_pp=diag_precon_pp*r_pp; beta=DOT_PRODUCT_P(r_pp,d_pp)/up
@@ -180,24 +163,22 @@ PROGRAM p122
        CALL checon_par(xnew_pp,cjtol,cj_converged,x_pp)
        IF(cj_converged.or.cjiters==cjits)EXIT
      END DO conjugate_gradients
-!---------------------------- end of pcg process -------------------------
      cjtot=cjtot+cjiters; loads_pp=xnew_pp; pmul_pp=zero
-!-----------------------   check plastic convergence  --------------------
+!----------------------- check plastic convergence -----------------------
      CALL checon_par(loads_pp,plastol,plastic_converged,oldis_pp)
      IF(plasiters==1)plastic_converged=.FALSE. 
      IF(plastic_converged.OR.plasiters==plasits)bdylds_pp=zero
      CALL gather(loads_pp,pmul_pp); utemp_pp=zero
-!------------------------ go round the Gauss Points ----------------------
      elements_5: DO iel=1,nels_pp
        bload=zero; eld=pmul_pp(:,iel)
        gauss_points_2: DO i=1,nip
          CALL shape_der(der,points,i); jac=MATMUL(der,g_coord_pp(:,:,iel))
-         det=determinant(jac); CALL invert(jac)
-         deriv=MATMUL(jac,der); CALL beemat(bee,deriv)
-         eps=MATMUL(bee,eld); eps=eps-evpt_pp(:,i,iel)
-         sigma=MATMUL(dee,eps); stress=sigma+tensor_pp(:,i,iel)
+         det=determinant(jac); CALL invert(jac); deriv=MATMUL(jac,der)
+         CALL beemat(bee,deriv); eps=MATMUL(bee,eld)
+         eps=eps-evpt_pp(:,i,iel); sigma=MATMUL(dee,eps)
+         stress=sigma+tensor_pp(:,i,iel)
          CALL invar(stress,sigm,dsbar,lode_theta)                            
-!----------------------  check whether yield is violated  ----------------
+!-------------------- check whether yield is violated --------------------
          CALL mocouf(phi,c,sigm,dsbar,lode_theta,f)
          IF(plastic_converged.OR.plasiters==plasits)THEN
            devp=stress 
@@ -206,23 +187,19 @@ PROGRAM p122
              CALL mocouq(psi,dsbar,lode_theta,dq1,dq2,dq3)
              CALL formm(stress,m1,m2,m3); flow=f*(m1*dq1+m2*dq2+m3*dq3)
              erate=MATMUL(flow,stress); evp=erate*dt
-             evpt_pp(:,i,iel)=evpt_pp(:,i,iel)+evp
-             devp=MATMUL(dee,evp)
+             evpt_pp(:,i,iel)=evpt_pp(:,i,iel)+evp; devp=MATMUL(dee,evp)
            END IF
          END IF
          IF(f>=zero)THEN
            eload=MATMUL(TRANSPOSE(bee),devp)
            bload=bload+eload*det*weights(i)
          END IF
-         IF(plastic_converged.OR.plasiters==plasits)THEN                       
-!---------------------- update the Gauss Point stresses  -----------------
+         IF(plastic_converged.OR.plasiters==plasits)                     &
            tensor_pp(:,i,iel)=stress          
-         END IF
        END DO gauss_points_2
-!-------------compute the total bodyloads vector -------------------------
+!------------------ compute the total bodyloads vector -------------------
        utemp_pp(:,iel)=utemp_pp(:,iel)+bload
-     END DO elements_5
-     CALL scatter(bdylds_pp,utemp_pp)
+     END DO elements_5; CALL scatter(bdylds_pp,utemp_pp)
      IF(plastic_converged.OR.plasiters==plasits)EXIT
    END DO plastic_iterations
    totd_pp=totd_pp+loads_pp
@@ -235,7 +212,7 @@ PROGRAM p122
      WRITE(11,'(A,F11.2)')"cj iterations per plastic iteration were ",    &
        REAL(cjtot)/REAL(plasiters)
    END IF
-!----------------------- write out the displacements ---------------------
+!----------------------- write out the displacements ----------------------
    IF(numpe==1)THEN; WRITE(ch,'(I6.6)') iy
      OPEN(12,file=argv(1:nlen)//".ensi.DISPL-"//ch,status='replace',      &
        action='write')
