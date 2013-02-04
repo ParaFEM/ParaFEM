745e795e8eede65b4fe5bf8be9e82c04c293237d
lee.margetts <lee.margetts@manchester.ac.uk>
2013-02-04 15:57:38 +0000

    Replaced p128_5.f90 (Arnoldi) with (Lanczos)

--- parafem/src/programs/5th_ed/p128_5/p128_5.f90
+++ parafem/src/programs/5th_ed/p128_5/p128_5.f90
@@ -1,171 +1,189 @@
-PROGRAM P128
+PROGRAM p128    
 !-------------------------------------------------------------------------
-!      program p12.8 eigenvalues and eigenvectors of a cuboidal elastic
-!      solid in 3d using uniform 8-node hexahedral elements ; Arnoldi "dp" 
+!      Program 10.4 eigenvalues and eigenvectors of a cuboidal elastic
+!      solid in 3d using  uniform 8-node hexahedral elements  
 !      for lumped mass this is done element by element : parallel version
 !-------------------------------------------------------------------------
- USE precision; USE global_variables; USE mp_interface; USE input
- USE output; USE loading; USE maths; USE gather_scatter; USE partition
- USE elements; USE steering; USE pcg; USE timing
+ USE new_library
+ USE geometry_lib
+ USE lancz_lib
+ USE precision
+ USE timing
+ USE utility
+ USE mp_module
+ USE global_variables1
+ USE gather_scatter6
  IMPLICIT NONE
-! neq,ntot are global variables - not declared
- INTEGER::nn,nr,nip,nodof=3,nod,nst=6,i,j,ii,jj,k,l,inode,iel,ndim=3,    &
-   iters,model,nconv,ncv,nev,maxitr,argc,iargc,partitioner=1,nels,ndof,  &
-   npes_pp,meshgen,first,last,ic,ido,ierr,info,iparam(11),ipntr(11),     &
-   ishfts,lworkl,nlen  
- REAL(iwp)::rho,e,nu,det,sigma,tol
- REAL(iwp),PARAMETER::zero=0.0_iwp, one=1.0_iwp  
- CHARACTER (LEN=15)::element; CHARACTER(LEN=50)::argv 
- CHARACTER(LEN=6)::ch; CHARACTER::bmat*1, which*2 
- LOGICAL::rvec
-!---------------------------- dynamic arrays -----------------------------
- REAL(iwp),ALLOCATABLE::points(:,:),dee(:,:),coord(:,:),vdiag(:),        &
-   resid(:),fun(:),jac(:,:),der(:,:),deriv(:,:),weights(:),bee(:,:),     &
-   store_km_pp(:,:,:),emm(:,:),ecm(:,:),diag(:),pmul(:),d(:,:),v(:,:),   &
-   workl(:),workd(:),g_coord_pp(:,:,:),diag1(:),udiag(:),utemp(:),       &
-   eigv(:,:),eigv1(:,:),timest(:)
- INTEGER, ALLOCATABLE::rest(:,:),g(:),num(:),g_num_pp(:,:),g_g_pp(:,:),  &
-   g_g_local(:,:),g_g(:,:),g_num(:,:),g_num_local(:,:)
- LOGICAL, ALLOCATABLE::select(:)   
- INCLUDE  'debug.h'     ! From ARPACK library
-!------------------------ input and initialisation -----------------------
- ALLOCATE(timest(20)); timest=zero; timest(1)=elap_time()
- CALL find_pe_procs(numpe,npes); CALL getname(argv,nlen) 
- CALL read_xx6(argv,numpe,bmat,e,element,maxitr,meshgen,ncv,nels,    &
-   nev,nip,nn,nod,nr,rho,tol,nu,which)
- CALL calc_nels_pp(argv,nels,npes,numpe,partitioner,nels_pp)
- ndof=nod*nodof; ntot=ndof
- ALLOCATE(g_num_pp(nod, nels_pp),g_coord_pp(nod,ndim,nels_pp),           &
-   rest(nr,nodof+1)); g_num_pp=0; g_coord_pp=zero; rest=0
- CALL read_g_num_pp2(argv,iel_start,nn,npes,numpe,g_num_pp)
- IF(meshgen == 2) CALL abaqus2sg(element,g_num_pp)
- CALL read_g_coord_pp(argv,g_num_pp,nn,npes,numpe,g_coord_pp)
- CALL read_rest(argv,numpe,rest); timest(2)=elap_time()
-!----------- initialise values of parameters required by ARPACK ----------
- lworkl=ncv*(ncv+8); ndigit=-3; logfil=6; msgets=0; mseupd=0; msaitr=0
- msaupd=1; msaup2=0; mseigt=0; msapps=0; info=0; ishfts=1; model=1; ido=0    
- iparam(1)=ishfts; iparam(3)=maxitr; iparam(7)=model
-!-------------- allocate dynamic arrays used in main program -------------
- ALLOCATE(points(nip,ndim),pmul(ntot),coord(nod,ndim),fun(nod),          &
-   jac(ndim,ndim), weights(nip),der(ndim,nod),deriv(ndim,nod),           &
-   dee(nst,nst),num(nod),store_km_pp(ntot,ntot,nels_pp),                 &
-   g_g_pp(ntot,nels_pp),ecm(ntot,ntot),utemp(ntot),d(ncv,2),             &
-   workl(lworkl),select(ncv),bee(nst,ntot),emm(ntot,ntot),g(ntot))    
-!----------  find the steering array and equations per process -----------
- CALL rearrange(rest); g_g_pp=0; neq=0
+!------------ ndof,nels,neq,ntot are global - not declared----------------
+ INTEGER::nxe,nye,nze,nn,nr,nip,nodof=3,nod=8,nst=6,i,j,k,iel,ndim=3,     &
+   nmodes,jflag,iflag=-1,lp=11,lalfa,leig,lx,lz,iters,neig=0,nlen
+ REAL(iwp)::aa,bb,cc,rho,e,v,det,el,er,acc  
+ CHARACTER(LEN=15)::element='hexahedron',argv
+!--------------------------- dynamic arrays-------------------------------
+ REAL(iwp),ALLOCATABLE::points(:,:),dee(:,:),coord(:,:),vdiag_pp(:),      &  
+   fun(:),jac(:,:),der(:,:),deriv(:,:),weights(:),bee(:,:),km(:,:),       &
+   emm(:,:),ecm(:,:),utemp_pp(:,:),ua_pp(:),va_pp(:),eig(:),del(:),       &
+   udiag_pp(:),diag_pp(:),alfa(:),beta(:),w1_pp(:),y_pp(:,:),z_pp(:,:),   &
+   pmul_pp(:,:),v_store_pp(:,:),p_g_co_pp(:,:,:),diag_tmp(:,:),x(:)
+ INTEGER,ALLOCATABLE::rest(:,:),g(:),num(:),g_num_pp(:,:),g_g_pp (:,:),   &
+   nu(:),jeig(:,:)
+!----------------------input and initialisation---------------------------
+ timest(1)=elap_time()
+ CALL find_pe_procs(numpe,npes)
+ IF(numpe==npes)THEN
+   CALL getname(argv,nlen)
+   OPEN(10,FILE=argv(1:nlen)//'.dat',STATUS='OLD',ACTION='READ')
+   OPEN(11,FILE=argv(1:nlen)//'.res',STATUS='REPLACE',ACTION='WRITE')              
+   READ(10,*)nels,nxe,nze,nip,aa,bb,cc,rho,e,v,nmodes,el,er,lalfa,leig,lx,&
+     lz,acc
+ END IF
+ CALL bcast_inputdata_p128(numpe,npes,nels,nxe,nze,nip,aa,bb,cc,rho,e,v,  &
+   nmodes,el,er,lalfa,leig,lx,lz,acc)
+ CALL calc_nels_pp
+ ndof=nod*nodof
+ ntot=ndof
+ nn=0
+ neq=0
+ nr=(nxe+1)*(nze+1)
+ nye=nels/nxe/nze
+ ALLOCATE(rest(nr,nodof+1),points(nip,ndim),pmul_pp(ntot,nels_pp),        &
+   coord(nod,ndim),fun(nod),jac(ndim,ndim),weights(nip),                  &
+   g_num_pp(nod,nels_pp),der(ndim,nod),deriv(ndim,nod),dee(nst,nst),      &
+   num(nod),km(ntot,ntot),g(ntot),g_g_pp(ntot,nels_pp),ecm(ntot,ntot),    &
+   eig(leig),x(lx),del(lx),nu(lx),jeig(2,leig),alfa(lalfa),beta(lalfa),   &
+   z_pp(lz,leig),utemp_pp(ntot,nels_pp),p_g_co_pp(nod,ndim,nels_pp),      &
+   bee(nst,ntot),emm(ntot,ntot),diag_tmp(ntot,nels_pp))
+ rest=0
+ DO i=1,nr
+   rest(i,1)=i
+ END DO
+ ielpe=iel_start
+!------------------ loop the elements to set up global arrays  -----------
  elements_1: DO iel=1,nels_pp
-    CALL find_g3(g_num_pp(:,iel),g_g_pp(:,iel),rest)
+   CALL geometry_8bxz(ielpe,nxe,nze,aa,bb,cc,coord,num)
+   CALL find_g(num,g,rest)
+   g_num_pp(:,iel)=num
+   p_g_co_pp(:,:,iel)=coord
+   g_g_pp(:,iel)=g
+   ielpe=ielpe+1
  END DO elements_1
- elements_2: DO iel=1,nels_pp  
-    i=MAXVAL(g_g_pp(:,iel)); IF(i>neq) neq=i
- END DO elements_2  
- neq=MAX_INTEGER_P(neq); CALL calc_neq_pp; CALL calc_npes_pp(npes,npes_pp)
- CALL make_ggl2(npes_pp,npes,g_g_pp)  
- ALLOCATE(v(neq,ncv),diag(neq),udiag(neq),vdiag(neq),workd(3*neq),       &
-   resid(neq),diag1(neq)); diag1=zero; v=zero; diag=zero; udiag=zero
-   vdiag=zero; workd=zero; resid=zero; diag1=zero
-!------ element stiffness integration and build the preconditioner -------
- CALL sample(element,points,weights); CALL deemat(e,nu,dee)
- store_km_pp=zero
- elements_3: DO iel=1,nels_pp
-   emm=zero
-   integrating_pts_1:  DO i=1,nip
-     CALL shape_fun(fun,points,i); CALL shape_der(der,points,i)
-     jac=MATMUL(der,g_coord_pp(:,:,iel)); det=determinant(jac) 
-     CALL invert(jac); deriv=MATMUL(jac,der); CALL beemat(deriv,bee)
-     store_km_pp(:,:,iel)=store_km_pp(:,:,iel)+                          & 
-                          MATMUL(MATMUL(TRANSPOSE(bee),dee),bee)*        &
-                          det*weights(i)
-     CALL ecmat(ecm,fun,ntot,nodof); emm=emm+ecm*det*weights(i)*rho
-   END DO integrating_pts_1
-   DO k=1,ntot; IF(g_g_pp(k,iel)/=0) THEN
-     diag1(g_g_pp(k,iel))=diag1(g_g_pp(k,iel))+SUM(emm(k,:)); END IF        
+ nn=(nxe+1)*(nze+1)*(nye+1)
+ neq=(nn-nr)*nodof
+ CALL calc_neq_pp
+ CALL make_ggl(g_g_pp)
+ IF(numpe==npes)THEN
+   WRITE(11,'(A,I5,A)')"This job ran on ",npes,"   processors"
+   WRITE(11,'(A)')"Global coordinates and node numbers"
+   DO i=1,nels_pp,nels_pp-1
+     WRITE(11,'(A,I8)')"Element ",i
+     num=g_num_pp(:,i)
+     DO k=1,nod
+       WRITE(11,'(A,I8,3E12.4)')"  Node",num(k),p_g_co_pp(k,:,i)
+     END DO
    END DO
- END DO elements_3
- CALL MPI_ALLREDUCE(diag1,diag,neq,MPI_REAL8,MPI_SUM,MPI_COMM_WORLD,ier)   
-!-------------------------- find the eigenvalues -------------------------
- iters=0; diag=one/sqrt(diag) ! diag holds l**(-1/2) 
- DO
-   iters=iters+1
-   CALL dsaupd(ido,bmat,neq,which,nev,tol,resid,                 &
-               ncv,v,neq,iparam,ipntr,workd,workl,lworkl,info) 
-   IF(ido/=-1 .AND. ido/=1) EXIT
-   diag1=zero; vdiag=workd(ipntr(1):ipntr(1)+neq-1)*diag
-   elements_4: DO iel=1,nels_pp
-     DO i=1,ntot
-       IF(g_g_pp(i,iel)==0) pmul(i)=zero
-       IF(g_g_pp(i,iel)/=0) pmul(i)=vdiag(g_g_pp(i,iel))
-     END DO
-     utemp=MATMUL(store_km_pp(:,:,iel),pmul)   
-!    CALL dgemv('n',ntot,ntot,one,km,ntot,pmul,1,zero,utemp,1) 
-     DO i=1,ntot
-       IF(g_g_pp(i,iel)/=0) THEN
-         diag1(g_g_pp(i,iel))=diag1(g_g_pp(i,iel))+utemp(i)
-       END IF
-     END DO
-   END DO elements_4
-   CALL MPI_ALLREDUCE(diag1,udiag,neq,MPI_REAL8,MPI_SUM,MPI_COMM_WORLD,  &
-     ier); udiag=udiag*diag; workd(ipntr(2):ipntr(2)+neq-1)=udiag
- END DO
- IF(numpe==1) THEN
-   OPEN(11,FILE=argv(1:nlen)//".res",STATUS='REPLACE',ACTION='WRITE')
-   WRITE(11,'(A,I8,A)') "It took ",iters,"  iterations"
+   WRITE(11,'(A,3(I8,A))')"There are ",nn," nodes",nr,                    &
+     " restrained  and",neq," equations"
+   WRITE(11,*)"Time after setup is  :",elap_time()-timest(1)
  END IF
- IF(info < 0) THEN ! Either we have convergence or there is an error
-   IF(numpe==1) WRITE(11,*) "Fatal error in dsaupd "
- ELSE
-   rvec=.TRUE.
-   CALL dseupd(rvec,'All',select,d,v,neq,sigma,bmat,neq,which,nev,tol,   &
-     resid,ncv,v,neq,iparam,ipntr,workd,workl,lworkl,ierr)
-   IF(ierr/=0) THEN; IF(numpe==1) THEN
-     WRITE(11,*) "Fatal error in dseupd "
-     WRITE(11,*) "Should compute residuals "
-   END IF; END IF
-   IF(numpe==1) THEN
-     WRITE(11,'(A)') "The eigenvalues are  :"
-     WRITE(11,'(5E12.4)') d(1:nev,1)
-     WRITE(11,'(A)') "The eigenvectors are  :"
-     DO i=1,nev
-        udiag(:)=v(1:neq,i); udiag=udiag*diag  
-        WRITE(11,'("Eigenvector number  ",I4," is: ")')  i
-        WRITE(11,'(6E12.4)') udiag(1:6)
-     END DO
-   END IF
- END IF
-!---------------------- write out the eigenvectors -----------------------   
- ALLOCATE(eigv(ndim,nn),g_num(nod,nels),g_num_local(nod,nels),           &
-   g_g(ntot,nels),g_g_local(ntot,nels))
- eigv=zero; g_num=0; g_num_local=0; g_g=0; g_g_local=0
- first=iel_start; last=iel_start+nels_pp-1
- g_num_local(:,first:last)=g_num_pp(:,:)
- g_g_local(:,first:last)=g_g_pp(:,:); ic=nod*nels 
- CALL MPI_ALLREDUCE(g_num_local,g_num,ic,MPI_INTEGER,MPI_SUM,            &
-   MPI_COMM_WORLD,ier)
- ic=ntot*nels
- CALL MPI_ALLREDUCE(g_g_local,g_g,ic,MPI_INTEGER,MPI_SUM,                &
-   MPI_COMM_WORLD,ier)
- DO i=1,nev
-   IF(numpe==1) THEN; WRITE(ch,'(I6.6)') i
-     OPEN(12,file=argv(1:nlen)//".ensi.DISPL-"//ch,status='replace',       &
-       action='write')
-     WRITE(12,'(A)') "Alya Ensight Gold --- Vector per-node variable file"
-     WRITE(12,'(A/A/A)') "part", "     1","coordinates"
-   END IF
-   eigv=zero; udiag=zero; udiag(:)=v(1:neq,i); udiag=udiag*diag  
-   DO iel=1,nels
-     DO j=1,nod; inode=g_num(j,iel)
-       DO k=1,ndim; l=((j-1)*ndim)+k
-         IF(g_g(l,iel)/=0) eigv(k,inode)=udiag(g_g(l,iel))
-   END DO; END DO; END DO
-   IF(numpe==1) THEN
-     DO ii=1,ndim
-       DO jj=1,nn
-         WRITE(12,('(E12.4)')) eigv(ii,jj)
-       END DO
-     END DO
-     CLOSE(12)
-   END IF
+ ALLOCATE(ua_pp(neq_pp),va_pp(neq_pp),vdiag_pp(neq_pp),                   &
+   v_store_pp(neq_pp,lalfa),diag_pp(neq_pp),udiag_pp(neq_pp),             &
+   w1_pp(neq_pp),y_pp(neq_pp,leig))
+ ua_pp=.0_iwp
+ va_pp=.0_iwp
+ eig=.0_iwp
+ diag_tmp=.0_iwp
+ jeig=0
+ x=.0_iwp
+ del=.0_iwp
+ nu=0
+ alfa=.0_iwp
+ beta=.0_iwp
+ diag_pp=.0_iwp
+ udiag_pp=.0_iwp
+ w1_pp=.0_iwp
+ y_pp=.0_iwp
+ z_pp=.0_iwp
+ CALL sample(element,points,weights)
+ CALL deemat(dee,e,v)
+!--------------- element stiffness integration and assembly---------------
+ elements_2: DO iel=1,nels_pp
+   coord=p_g_co_pp(:,:,iel)
+   g=g_g_pp(:,iel)
+   km=0.0_iwp
+   emm=0.0_iwp
+   integrating_pts_1: DO i=1,nip
+     CALL shape_fun(fun,points,i)
+     CALL shape_der(der,points,i)
+     jac=MATMUL(der,coord)
+     det=determinant(jac)
+     CALL invert(jac)
+     deriv=MATMUL(jac,der)
+     CALL beemat(bee,deriv)
+     km=km+MATMUL(MATMUL(TRANSPOSE(bee),dee),bee)*det*weights(i)
+     CALL ecmat(ecm,fun,ntot,nodof)
+     emm=emm+ecm*det*weights(i)*rho
+   END DO integrating_pts_1
+   DO k=1,ntot
+     diag_tmp(k,iel)=diag_tmp(k,iel)+sum(emm(k,:))
+   END DO
+ END DO elements_2
+ CALL scatter(diag_pp,diag_tmp)
+ DEALLOCATE(diag_tmp)
+!------------------------------find eigenvalues---------------------------
+ diag_pp=1._iwp/sqrt(diag_pp) ! diag_pp holds l**(-1/2)
+ DO iters=1,lalfa
+   CALL lancz1(neq_pp,el,er,acc,leig,lx,lalfa,lp,iflag,ua_pp,va_pp,eig,   &
+     jeig,neig,x,del,nu,alfa,beta,v_store_pp)
+   IF(iflag==0)EXIT
+   IF(iflag>1)THEN  
+     IF(numpe==npes)THEN        
+       WRITE(11,'(A,I5)')                                                 &
+         " Lancz1 is signalling failure, with iflag = ",iflag
+       EXIT
+     END IF 
+   END IF           
+!---- iflag = 1 therefore form u + a * v  ( done element by element )-----
+   vdiag_pp=va_pp
+   vdiag_pp=vdiag_pp*diag_pp  ! vdiag is l**(-1/2).va
+   udiag_pp=.0_iwp
+   pmul_pp=.0_iwp
+   CALL gather(vdiag_pp,pmul_pp)
+   elements_3: DO iel=1,nels_pp
+!    utemp_pp(:,iel) = MATMUL(km,pmul_pp(:,iel))
+     CALL dgemv('n',ntot,ntot,1.0,km,ntot,pmul_pp(:,iel),1,0.0,           &
+       utemp_pp(:,iel),1)
+   END DO elements_3
+   CALL scatter(udiag_pp,utemp_pp)   ! udiag is A.l**(-1/2).va
+   udiag_pp=udiag_pp*diag_pp
+   ua_pp=ua_pp+udiag_pp
  END DO
- IF(numpe==1) WRITE(11,*) "This analysis took  :", elap_time( ) - timest(1)
- CALL shutdown()
- END PROGRAM P128
+!-------------- iflag = 0 therefore write out the spectrum --------------- 
+ IF(numpe==npes)THEN 
+   WRITE(11,'(2(A,E12.4))')"The range is",el,"  to ",er
+   WRITE(11,'(A,I8,A)')"There are ",neig," eigenvalues in the range"
+   WRITE(11,'(A,I8,A)')"It took ",iters,"  iterations"
+   WRITE(11,'(A)')"The eigenvalues are   :"
+   WRITE(11,'(6E12.4)')eig(1:neig)  
+ END IF     
+!  calculate the eigenvectors
+ IF(neig>10)neig=10
+ CALL lancz2(neq_pp,lalfa,lp,eig,jeig,neig,alfa,beta,lz,jflag,y_pp,w1_pp, &
+   z_pp,v_store_pp)
+!------------------if jflag is zero  calculate the eigenvectors ----------
+ IF(jflag==0)THEN   
+   IF(numpe==npes)THEN 
+     WRITE(11,'(A)')"The eigenvectors are  :"  
+     DO i=1,nmodes
+       udiag_pp(:)=y_pp(:,i)
+       udiag_pp=udiag_pp*diag_pp
+       WRITE(11,'("Eigenvector number  ",I4," is: ")')i
+       WRITE(11,'(6E12.4)')udiag_pp(1:6)
+     END DO
+   ELSE
+! lancz2 fails
+     WRITE(11,'(A,I5)')" Lancz2 is signalling failure with jflag = ",jflag  
+   END IF 
+ END IF
+ IF(numpe==npes)WRITE(11,*)"This analysis took  :",elap_time()-timest(1)
+ CALL shutdown()  
+END PROGRAM p128
