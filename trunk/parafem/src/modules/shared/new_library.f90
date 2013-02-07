d1697230f2c1f8ac3cb58be87bf7b9be84bc214a
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2013-02-07 23:26:23 +0000

    Added subroutine CHECON to new_library.f90

--- trunk/parafem/src/modules/shared/new_library.f90
+++ trunk/parafem/src/modules/shared/new_library.f90
@@ -3724,4 +3724,24 @@ end subroutine formupvw
  RETURN
  END SUBROUTINE num_to_g 
 
+!---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
+ 
+ SUBROUTINE checon(loads,oldlds,tol,converged)
+ !
+ ! This subroutine sets converged to .FALSE. if relative change in loads
+ ! and oldlds is greater than tol and updates oldlds.
+ !
+  IMPLICIT NONE
+  INTEGER,PARAMETER::iwp=SELECTED_REAL_KIND(15)
+  REAL(iwp),INTENT(IN)::loads(0:),tol
+  REAL(iwp),INTENT(IN OUT)::oldlds(0:)
+  LOGICAL,INTENT(OUT)::converged
+  CONVERGED=.TRUE.
+  CONVERGED=(MAXVAL(ABS(loads-oldlds))/MAXVAL(ABS(loads))<=tol)
+  oldlds=loads
+ RETURN
+END SUBROUTINE checon
+
 END MODULE NEW_LIBRARY
