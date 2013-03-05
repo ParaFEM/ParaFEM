03b91908c6d907a5529452bc1dba01bc30ade1bb
lee.margetts <lee.margetts@manchester.ac.uk>
2013-03-05 15:46:28 +0000

    Added subroutine LOAD_FUNCTION to new_library.f90

--- parafem/src/modules/shared/new_library.f90
+++ parafem/src/modules/shared/new_library.f90
@@ -3744,4 +3744,38 @@ end subroutine formupvw
  RETURN
 END SUBROUTINE checon
 
+!---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
+
+SUBROUTINE load_function(lf,dtim,al)
+!
+! This subroutine forms the increment of load at each
+! calculation time step.
+!
+ IMPLICIT NONE
+ INTEGER,PARAMETER::iwp=SELECTED_REAL_KIND(15)
+ REAL(iwp),INTENT(IN)::lf(:,:),dtim
+ REAL(iwp),INTENT(IN OUT)::al(:)
+ REAL::time,aold,anew
+ INTEGER::nlfp,nincs,i,j
+ nincs=SIZE(al)
+ nlfp=UBOUND(lf,2)
+ aold=lf(2,1)                        
+ time=lf(1,1)
+ DO i=1,nincs
+   time=time+dtim
+   DO j=1,nlfp
+     IF(time<lf(1,j))THEN
+       anew=lf(2,j-1)+                                                    &
+         (time-lf(1,j-1))*(lf(2,j)-lf(2,j-1))/(lf(1,j)-lf(1,j-1))
+       al(i)=anew-aold
+       aold=anew
+       EXIT
+     END IF
+   END DO
+ END DO
+RETURN
+END SUBROUTINE load_function
+
 END MODULE NEW_LIBRARY
