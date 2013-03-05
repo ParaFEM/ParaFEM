940b1c92143bd8c961339b7435abb8d9cb9fde7d
lee.margetts <lee.margetts@manchester.ac.uk>
2013-03-05 16:52:25 +0000

    Added subroutine FORMKE to new_library.f90

--- parafem/src/modules/shared/new_library.f90
+++ parafem/src/modules/shared/new_library.f90
@@ -3778,4 +3778,25 @@ SUBROUTINE load_function(lf,dtim,al)
 RETURN
 END SUBROUTINE load_function
 
+!---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
+!---------------------------------------------------------------------------
+
+SUBROUTINE formke(km,kp,c,ke,theta)
+!
+! This subroutine creates the ke matrix for incremental Biot.
+!
+ IMPLICIT NONE
+ INTEGER,PARAMETER::iwp=SELECTED_REAL_KIND(15)
+ REAL(iwp),INTENT(IN)::km(:,:),kp(:,:),c(:,:),theta
+ REAL(iwp),INTENT(OUT)::ke(:,:)
+ INTEGER::ndof
+ ndof=UBOUND(km,1)
+ ke(:ndof,:ndof)=km 
+ ke(:ndof,ndof+1:)=c
+ ke(ndof+1:,:ndof)=TRANSPOSE(c) 
+ ke(ndof+1:,ndof+1:)=-theta*kp
+RETURN  
+END SUBROUTINE formke
+
 END MODULE NEW_LIBRARY
