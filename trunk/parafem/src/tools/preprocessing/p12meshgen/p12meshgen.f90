f0ed0c101b92bddcef94a4b0defa2953be31de1d
lee.margetts@manchester.ac.uk <lee.margetts@manchester.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2013-11-06 21:09:18 +0000

    Restructured the p122 section in p12meshgen.f90

--- trunk/parafem/src/tools/preprocessing/p12meshgen/p12meshgen.f90
+++ trunk/parafem/src/tools/preprocessing/p12meshgen/p12meshgen.f90
@@ -365,10 +365,6 @@ PROGRAM p12meshgen
 ! p122.1  Select 8 node or 20 node hexahedra
 !------------------------------------------------------------------------------
 
-    SELECT CASE(iotype)
-
-    CASE('parafem')
-
     SELECT CASE(nod)
    
 !------------------------------------------------------------------------------
@@ -407,7 +403,6 @@ PROGRAM p12meshgen
   
 !------------------------------------------------------------------------------
 ! p122.4  Find nodal coordinates and element steering array
-!         Write to file using Abaqus node numbering convention 
 !------------------------------------------------------------------------------
 
      DO iel = 1, nels
@@ -415,96 +410,21 @@ PROGRAM p12meshgen
        g_coord(:,g_num(:,iel)) = TRANSPOSE(coord)
      END DO
   
-     OPEN(11,FILE=argv(1:nlen)//'.d',STATUS='REPLACE',ACTION='WRITE')
-          
-     WRITE(11,'(A)') "*THREE_DIMENSIONAL"
-     WRITE(11,'(A)') "*NODES"
-  
-     DO i = 1, nn
-       WRITE(11,'(I12,3E14.6)') i, g_coord(:,i)
-     END DO
-  
-     WRITE(11,'(A)') "*ELEMENTS"
-    
-     DO iel = 1, nels
-       WRITE(11,'(I12,A,20I12,A)')iel," 3 20 1 ",g_num(3,iel),g_num(5,iel),   &
-                                  g_num(7,iel),g_num(1,iel),g_num(15,iel),    &
-                                  g_num(17,iel),g_num(19,iel),g_num(13,iel),  &
-                                  g_num(4,iel),g_num(6,iel),g_num(8,iel),     &
-                                  g_num(2,iel),g_num(16,iel),g_num(18,iel),   &
-                                  g_num(20,iel),g_num(14,iel),g_num(10,iel),  &
-                                  g_num(11,iel),g_num(12,iel),g_num(9,iel),   &
-                                  " 1"
-     END DO
-  
-     CLOSE(11)
-  
 !------------------------------------------------------------------------------
 ! p122.5  Boundary conditions
 !------------------------------------------------------------------------------
     
-     OPEN(12,FILE=argv(1:nlen)//'.bnd',STATUS='REPLACE',ACTION='WRITE')
- 
      CALL cube_bc20(rest,nxe,nye,nze)
-  
-     DO i = 1, nr
-       WRITE(12,'(I12,3I2)') rest(i,:) 
-     END DO
-  
-     CLOSE(12)
   
 !------------------------------------------------------------------------------
 ! p122.6  Loading conditions
 !------------------------------------------------------------------------------
 
-    OPEN(13,FILE=argv(1:nlen)//'.lds',STATUS='REPLACE',ACTION='WRITE')
-  
-    IF(problem_type == 'ed4') THEN
-      CALL load_p121(nle,nod,nxe,nze, no,val)
-!     val = -val * aa * bb * (25._iwp / 12._iwp)
-      val = -val * aa * bb / 12._iwp
-      DO i = 1, loaded_freedoms
-        WRITE(13,'(I12,2A,3E16.8)') no(i),"  0.00000000E+00  ",              &
-                                   "0.00000000E+00",val(i) 
-      END DO
-    ELSE IF (problem_type == 'boussinesq') THEN
-      no  = 1
-      val = -1.0_iwp
-      DO i = 1, loaded_freedoms
-        WRITE(13,'(I12,2A,3E16.8)') no(i),"  0.00000000E+00  ",              &
-                                   "0.00000000E+00",val(i) 
-      END DO
-    ELSE
-      PRINT *, "Problem type: ", problem_type, " not recognised.            &&
-               & No values written to .lds"
-    END IF
-    
-    CLOSE(13)
+    CALL load_p121(nle,nod,nxe,nze, no,val)
+    val = -val * aa * bb / 12._iwp
   
 !------------------------------------------------------------------------------
-! p122.7  New control data
-!------------------------------------------------------------------------------
-  
-    OPEN(14,FILE=argv(1:nlen)//'.dat',STATUS='REPLACE',ACTION='WRITE')
-  
-    WRITE(14,'(A)') "'hexahedron'"
-    IF(nod==8) THEN
-      WRITE(14,'(A)') "1"            ! Abaqus node numbering scheme
-    ELSE
-      WRITE(14,'(A)') "2"            ! Abaqus node numbering scheme
-    END IF
-    WRITE(14,'(A)') "1"              ! Internal mesh partitioning
-    WRITE(14,'(5I9,A,I9)') nels, nn, nr, nip, nod, "  0  ",loaded_freedoms
-    WRITE(14,'(6E12.4,I8,A)') phi, c, psi, e, v
-    WRITE(14,'(3I6,2E12.4)') incs, plasits, cjits, plastol, cjtol
-    DO i=1,incs
-      WRITE(14,'(E12.4)') qinc(i)
-    END DO
-     
-    CLOSE(14)
-
-!------------------------------------------------------------------------------
-! p122.8  Create input deck for 8 node hexahedra
+! p122.7  Create input deck for 8 node hexahedra
 !------------------------------------------------------------------------------
     
    CASE(8)
@@ -526,7 +446,7 @@ PROGRAM p12meshgen
      END IF
 
 !------------------------------------------------------------------------------
-! p122.9  Allocate dynamic arrays
+! p122.8  Allocate dynamic arrays
 !------------------------------------------------------------------------------
 
      ALLOCATE(coord(nod,ndim),g_coord(ndim,nn),g_num(nod,nels),               &
@@ -537,8 +457,7 @@ PROGRAM p12meshgen
      g_num    = 0       ; rest    = 0       ;   no  = 0       ; num = 0
 
 !------------------------------------------------------------------------------
-! p122.10  Find nodal coordinates and element steering array
-!          Write to file using Abaqus node numbering convention 
+! p122.9  Find nodal coordinates and element steering array
 !------------------------------------------------------------------------------
 
      DO iel = 1, nels
@@ -546,75 +465,104 @@ PROGRAM p12meshgen
        g_coord(:,g_num(:,iel)) = TRANSPOSE(coord)
      END DO
 
+!------------------------------------------------------------------------------
+! p122.10  Boundary conditions
+!------------------------------------------------------------------------------
+
+     CALL cube_bc8(rest,nxe,nye,nze)
+  
+!------------------------------------------------------------------------------
+! p122.11  Loading conditions
+!------------------------------------------------------------------------------
+
+     CALL load_p121(nle,nod,nxe,nze, no,val)
+     val = val * aa * bb 
+
+!------------------------------------------------------------------------------
+! p122.12  Default case and error message
+!------------------------------------------------------------------------------
+  
+     CASE DEFAULT
+  
+     PRINT *
+     PRINT *, "Wrong value given in variable NOD"
+     PRINT *, "  Accepted values are 8 and 20"
+     PRINT *, "  Here NOD = ", nod
+     PRINT *
+      
+   END SELECT
+
+!------------------------------------------------------------------------------
+! p122.13  Output model
+!------------------------------------------------------------------------------
+
+   SELECT CASE(iotype)
+
+   CASE('parafem')
+   
+!------------------------------------------------------------------------------
+! p122.14  Output geometry file ".d" 
+!------------------------------------------------------------------------------
+         
      OPEN(11,FILE=argv(1:nlen)//'.d',STATUS='REPLACE',ACTION='WRITE')
-        
+       
      WRITE(11,'(A)') "*THREE_DIMENSIONAL"
      WRITE(11,'(A)') "*NODES"
-  
+     
      DO i = 1, nn
        WRITE(11,'(I12,3E14.6)') i, g_coord(:,i)
      END DO
-  
+     
      WRITE(11,'(A)') "*ELEMENTS"
-    
-     DO iel = 1, nels
-!      WRITE(11,'(I12,A,8I12,A)') iel, " 3 8 1 ", g_num(1,iel),g_num(4,iel),  &
-!                                   g_num(8,iel),g_num(5,iel),g_num(2,iel),   &
-!                                   g_num(3,iel),g_num(7,iel),g_num(6,iel),   &
-!                                   " 1"
-       WRITE(11,'(I12,A,8I12,A)') iel, " 3 8 1 ", g_num(1,iel),g_num(2,iel),  &
-                                    g_num(3,iel),g_num(4,iel),g_num(5,iel),   &
-                                    g_num(6,iel),g_num(7,iel),g_num(8,iel),   &
-                                    " 1"
-     END DO
-    
+       
+     IF(nod==20) THEN
+       DO iel = 1, nels
+          WRITE(11,'(I12,A,20I12,A)')iel," 3 20 1 ",g_num(3,iel),        &
+             g_num(5,iel),g_num(7,iel),g_num(1,iel),g_num(15,iel),       &
+             g_num(17,iel),g_num(19,iel),g_num(13,iel),g_num(4,iel),     &
+             g_num(6,iel),g_num(8,iel),g_num(2,iel),g_num(16,iel),       &
+             g_num(18,iel),g_num(20,iel),g_num(14,iel),g_num(10,iel),    &
+             g_num(11,iel),g_num(12,iel),g_num(9,iel)," 1"
+       END DO
+     ELSE IF(nod==8) THEN
+       DO iel = 1, nels
+         WRITE(11,'(I12,A,8I12,A)') iel, " 3 8 1 ", g_num(1,iel),        &
+             g_num(2,iel),g_num(3,iel),g_num(4,iel),g_num(5,iel),        &
+             g_num(6,iel),g_num(7,iel),g_num(8,iel)," 1"
+       END DO
+     ELSE
+         PRINT *, "Wrong number of nodes in element"
+     END IF
+     
      CLOSE(11)
 
 !------------------------------------------------------------------------------
-! p122.11  Boundary conditions
+! p122.15  Output ".bnd" file
 !------------------------------------------------------------------------------
 
      OPEN(12,FILE=argv(1:nlen)//'.bnd',STATUS='REPLACE',ACTION='WRITE')
-  
-     CALL cube_bc8(rest,nxe,nye,nze)
-  
+     
      DO i = 1, nr
        WRITE(12,'(I8,3I6)') rest(i,:) 
      END DO
-  
+     
      CLOSE(12)
 
 !------------------------------------------------------------------------------
-! p122.12  Loading conditions
+! p122.16  Output ".lds" file
 !------------------------------------------------------------------------------
 
      OPEN(13,FILE=argv(1:nlen)//'.lds',STATUS='REPLACE',ACTION='WRITE')
-  
-     IF(problem_type == 'ed4') THEN
-       CALL load_p121(nle,nod,nxe,nze, no,val)
-       val = val * aa * bb 
-       DO i = 1, loaded_freedoms
-         WRITE(13,'(I10,2A,3E16.8)') no(i),                                   &
-                                    "  0.00000000E+00  ","0.00000000E+00",    &
-                                     val(i) 
-       END DO
-     ELSE IF (problem_type == 'boussinesq') THEN
-       no  = 1
-       val = -1.0_iwp
-       DO i = 1, loaded_freedoms
-         WRITE(13,'(I10,2A,3E16.8)') no(i),                                   &
-                                    "  0.00000000E+00  ","0.00000000E+00",    &
-                                     val(i) 
-       END DO
-     ELSE
-       PRINT *, "Problem type: ", problem_type, " not recognised.            &&
-                & No values written to .lds"
-     END IF
-    
+     
+     DO i = 1, loaded_freedoms
+       WRITE(13,'(I12,2A,3E16.8)') no(i),"  0.00000000E+00  ",           &
+                                   "0.00000000E+00",val(i) 
+     END DO
+       
      CLOSE(13)
 
 !------------------------------------------------------------------------------
-! p122.13  New control data
+! p122.17  New control data
 !------------------------------------------------------------------------------
 
      OPEN(14,FILE=argv(1:nlen)//'.dat',STATUS='REPLACE',ACTION='WRITE')
@@ -634,20 +582,10 @@ PROGRAM p12meshgen
      END DO
      
      CLOSE(14)
-
+             
 !------------------------------------------------------------------------------
-! p122.14  Default case and error message
+! p122.18  Output for visualization in ParaView (Ensight Gold)
 !------------------------------------------------------------------------------
-  
-     CASE DEFAULT
-  
-     PRINT *
-     PRINT *, "Wrong value given in variable NOD"
-     PRINT *, "  Accepted values are 8 and 20"
-     PRINT *, "  Here NOD = ", nod
-     PRINT *
-      
-   END SELECT
   
    CASE('paraview')
 
