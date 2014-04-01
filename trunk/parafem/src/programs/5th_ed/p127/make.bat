839443408e2774cf3498544e41190ae7e8d74313
j.robinson@software.ac.uk <j.robinson@software.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2014-04-01 15:44:06 +0000

    Initial commit of build scripts for Windows demos

---
+++ trunk/parafem/src/programs/5th_ed/p127/make.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set DRIVER_NAME=p127
+set PARAFEM=c:\parafem\parafem
+set G95=mpif77 /integer-size:32 /real-size:64 /Od
+set FFLAGS=-c -I%PARAFEM%\include
+set LIB=%LIB%;%PARAFEM%\lib
+
+IF "%1"=="clean" (
+	del *.obj
+	del *.exe
+) else (
+	"Building %DRIVER_NAME% RELEASE"
+	%G95% %FFLAGS% *.f90
+	%G95% *.obj libparafem.lib -o %DRIVER_NAME%.exe  
+	copy *.exe %PARAFEM%\bin
+)
