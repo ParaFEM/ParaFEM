8184cd333b23caf34f5f7c7b58bb47914c5e12de
j.robinson@software.ac.uk <j.robinson@software.ac.uk@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2014-04-08 20:53:54 +0000

    Add background image to launcher. Fix Paraview launch path.  Add driver name to batch file header

--- trunk/demo-installer-win/demo-driver.bat
+++ trunk/demo-installer-win/demo-driver.bat
@@ -1,11 +1,13 @@
 @ECHO OFF
+SET PARAFEM_HOME=@INSTDIR@
 PATH=%PARAFEM_HOME%\OpenMPI_v1.6.2-win32\bin;%PATH%
+cd "%PARAFEM_HOME%\bin"
+
 ECHO
 ECHO -------------------------------------------------------------------------------
-ECHO  %1 - IF PROMPTED TO ALLOW FIREWALL ACCESS, PLEASE DO SO
+ECHO ParaFEM DEMO %1 - IF PROMPTED TO ALLOW FIREWALL ACCESS, PLEASE DO SO
 ECHO -------------------------------------------------------------------------------
-SET PARAFEM_HOME=C:\ParaFEM_Demo
-cd "%PARAFEM_HOME%\bin"
+ECHO. 
 
 mpirun --mca btl self,sm -np 1 %1.exe ..\examples\%1\demo\%1_demo
 rem mpirun --mca btl self,sm -np %NUMBER_OF_PROCESSORS% ..\examples\%1\demo\%1_demo
@@ -15,5 +17,5 @@ ECHO ---------------------------------------------------------------------------
 ECHO Execution complete.  You can now view the output in ParaView
 ECHO -------------------------------------------------------------------------------
 pause
-"%PARAFEM_HOME%\ParaView 4.1.0\bin\paraview" --state=%PARAFEM_HOME%\examples\%1\demo\%1_demo.pvsm
+"%PARAFEM_HOME%\ParaView 4.1.0\bin\paraview" --state="%PARAFEM_HOME%\examples\%1\demo\%1_demo.pvsm"
 
