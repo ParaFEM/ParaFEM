959472094a1ac9875d2e4f46824596b90c27d40b
shterenlikht@gmail.com <shterenlikht@gmail.com@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2014-10-15 15:38:32 +0000

    - fixed one error in src/modules/mpi/input.f900 still does not compile, there is at least one more error, but want to commit it separately
    - minor updates to UoB bc3 cluster and xx14
    cgpack/parafem dev program

--- trunk/parafem/examples/dev/xx14/readme.txt
+++ trunk/parafem/examples/dev/xx14/readme.txt
@@ -1,21 +1,28 @@
-EXAMPLES: xx14_<size>.mg
+ DESCRIPTION:	The files in this directory, xx14_<size>.mg,
+		are input files for the program p12meshgen.f90.
 
-The files in this directory are input files for the program p12meshgen.f90
+       USAGE:	Using the input file <filename>.mg,
+		program p12meshgen.f90 will create a full 
+		input deck for program xx14:
 
-Using the input file <filename>.mg, program p12meshgen.f90 will create a full 
-input deck for program xx14. 
+		p12meshgen xx14_medium
 
-     $ p12meshgen xx14_medium
+		by default, the p12meshgen executable can be found
+		under parafem/bin:
+
+		../../../../parafem/bin/p12meshgen xx14_medium
  
-     Will create an input deck for the problem described on pages 536-541 in
-     Chapter 12 of Smith, Griffiths and Margetts "Programming the Finite 
-     Element Method", 5th Edition, Wiley, 2014
+		Then the executable xx14 can be run
+		on the produced input deck.
+		Use like this (change options as required):
 
-AUTHOR
+		mpirun -np 16 xx14 xx14_medium
 
-lee.margetts@manchester.ac.uk
+  REFERENCES:	See the problem described on pages 536-541 in
+		Chapter 12 of Smith, Griffiths and Margetts
+		"Programming the Finite Element Method",
+		5th Edition, Wiley, 2014
 
-LAST UPDATED
+LAST UPDATED:	3-OCT-2014
 
-27 January 2014
-8-SEP-2014
+      AUTHOR:	Anton Shterenlikht, mexas@bris.ac.uk
--- trunk/parafem/src/programs/dev/xx14/xx14.f90
+++ trunk/parafem/src/programs/dev/xx14/xx14.f90
@@ -12,30 +12,51 @@ PROGRAM xx14
 !------------------------------------------------------------------------- 
 !USE mpi_wrapper  !remove comment for serial compilation
 
-!*********************************************************************72
+!*** CGPACK part *****************************************************72
 ! The CGPACK module must be used
  use cgca
-!*********************************************************************72
+!*** end CGPACK part *************************************************72
 
- USE precision; USE global_variables; USE mp_interface; USE input
- USE output; USE loading; USE timing; USE maths; USE gather_scatter
- USE steering; USE new_library; IMPLICIT NONE
+USE precision
+USE global_variables
+USE mp_interface
+USE input
+USE output
+USE loading
+USE timing
+USE maths
+USE gather_scatter
+USE steering
+USE new_library
+
+IMPLICIT NONE
+
 ! neq,ntot are now global variables - must not be declared
- INTEGER,PARAMETER::nodof=3,ndim=3,nst=6
- INTEGER::loaded_nodes,iel,i,j,k,iters,limit,nn,nr,nip,nod,nels,ndof,    &
-   npes_pp,node_end,node_start,nodes_pp,meshgen,partitioner,nlen
- REAL(iwp),PARAMETER::zero=0.0_iwp
- REAL(iwp)::e,v,det,tol,up,alpha,beta,q; LOGICAL::converged=.false.
- CHARACTER(LEN=50)::argv; CHARACTER(LEN=15)::element; CHARACTER(LEN=6)::ch 
-!---------------------------- dynamic arrays -----------------------------
- REAL(iwp),ALLOCATABLE::points(:,:),dee(:,:),weights(:),val(:,:),        &
-   disp_pp(:),g_coord_pp(:,:,:),jac(:,:),der(:,:),deriv(:,:),bee(:,:),   &
-   storkm_pp(:,:,:),eps(:),sigma(:),diag_precon_pp(:),p_pp(:),r_pp(:),   &
-   x_pp(:),xnew_pp(:),u_pp(:),pmul_pp(:,:),utemp_pp(:,:),d_pp(:),        &
-   timest(:),diag_precon_tmp(:,:),eld_pp(:,:),temp(:)
- INTEGER,ALLOCATABLE::rest(:,:),g_num_pp(:,:),g_g_pp(:,:),node(:)
 
-!*********************************************************************72
+INTEGER, PARAMETER :: nodof = 3, ndim = 3, nst = 6
+INTEGER :: loaded_nodes, iel, i, j, k, iters, limit, nn, nr, nip, nod,   &
+   nels, ndof, npes_pp, node_end, node_start, nodes_pp, meshgen,         &
+   partitioner, nlen
+
+REAL( iwp ), PARAMETER :: zero=0.0_iwp
+REAL( iwp ) :: e, v, det, tol, up, alpha, beta, q
+
+LOGICAL :: converged=.false.
+
+CHARACTER( LEN=50 ) :: argv
+CHARACTER( LEN=15 ) :: element
+CHARACTER( LEN=6 ) :: ch 
+
+!---------------------------- dynamic arrays -----------------------------
+REAL( iwp ), ALLOCATABLE :: points(:,:), dee(:,:), weights(:),val(:,:),  &
+   disp_pp(:), g_coord_pp(:,:,:), jac(:,:), der(:,:), deriv(:,:),        &
+   bee(:,:), storkm_pp(:,:,:), eps(:), sigma(:), diag_precon_pp(:),      &
+   p_pp(:), r_pp(:), x_pp(:), xnew_pp(:), u_pp(:), pmul_pp(:,:),         &
+   utemp_pp(:,:), d_pp(:), timest(:), diag_precon_tmp(:,:), eld_pp(:,:), &
+   temp(:)
+INTEGER, ALLOCATABLE :: rest(:,:), g_num_pp(:,:), g_g_pp(:,:), node(:)
+
+!*** CGPACK part *****************************************************72
 ! CGPACK variables and parameters
 integer( kind=idef ) :: cgca_ir(3), cgca_img, cgca_nimgs, cgca_lres, &
  cgca_ng                          ! number of grains in the whole model
@@ -49,12 +70,12 @@ real( kind=rdef ) ::    &
  cgca_res,              & ! resolutions, cells per grain
  cgca_bcol(3),          & ! lower phys. coords of the coarray on image
  cgca_bcou(3)             ! upper phys. coords of the coarray on image
-! End of CGPACK variables and parameters
-!*********************************************************************72
+!*** end CGPACK part *************************************************72
 
-!*********************************************************************72
+!*** CGPACK part *****************************************************72
 ! CGPACK commands
 
+! *** first executable statement ***
 ! physical dimensions of the box, assume mm
 cgca_bsz = (/ 1.0, 2.0, 3.0 /)
 
@@ -90,8 +111,7 @@ write ( *, "(9(a,i0),tr1,g0,tr1,i0,3(a,g0),a)" )               &
 call cgca_as( 1, cgca_c(1),  1, cgca_c(2),  1, cgca_c(3),      &
               1, cgca_ir(1), 1, cgca_ir(2), 1, 1, cgca_space )
 
-! calculate the phys. dim. of the coarray
-! on each image
+! calculate the phys. dim. of the coarray on each image
 !subroutine cgca_imco( space, lres, bcol, bcou )
 call cgca_imco( cgca_space, cgca_lres, cgca_bcol, cgca_bcou )
 
@@ -101,27 +121,37 @@ write ( *,"(a,i0,2(a,3(g0,tr1)),a)" ) "img: ", cgca_img,       &
 ! deallocate space
 call cgca_ds( cgca_space )
 
-! End of CGPACK commands
-!*********************************************************************72
+!*** end CGPACK part *************************************************72
 
 
 !------------------------ input and initialisation -----------------------
- ALLOCATE(timest(20)); timest=zero; timest(1)=elap_time()
- CALL find_pe_procs(numpe,npes); CALL getname(argv,nlen) 
- CALL read_p121(argv,numpe,e,element,limit,loaded_nodes,meshgen,nels,    &
-   nip,nn,nod,nr,partitioner,tol,v)
- CALL calc_nels_pp(argv,nels,npes,numpe,partitioner,nels_pp)
- ndof=nod*nodof; ntot=ndof
- ALLOCATE(g_num_pp(nod, nels_pp),g_coord_pp(nod,ndim,nels_pp),           &
-   rest(nr,nodof+1)); g_num_pp=0; g_coord_pp=zero; rest=0
- CALL read_g_num_pp(argv,iel_start,nn,npes,numpe,g_num_pp)
- IF(meshgen == 2) CALL abaqus2sg(element,g_num_pp)
- CALL read_g_coord_pp(argv,g_num_pp,nn,npes,numpe,g_coord_pp)
- CALL read_rest(argv,numpe,rest); timest(2)=elap_time()
- ALLOCATE(points(nip,ndim),dee(nst,nst),jac(ndim,ndim),der(ndim,nod),    &
-   deriv(ndim,nod),bee(nst,ntot),weights(nip),eps(nst),sigma(nst),       &
-   storkm_pp(ntot,ntot,nels_pp),pmul_pp(ntot,nels_pp),                   &
-   utemp_pp(ntot,nels_pp),g_g_pp(ntot,nels_pp))
+ALLOCATE( timest( 20 ) )
+timest = zero
+timest( 1 ) = elap_time()
+CALL find_pe_procs( numpe, npes )
+CALL getname( argv, nlen ) 
+CALL read_p121( argv, numpe, e, element, limit, loaded_nodes, meshgen, &
+   nels, nip, nn, nod, nr, partitioner, tol, v )
+CALL calc_nels_pp( argv, nels, npes, numpe, partitioner, nels_pp )
+ndof = nod * nodof
+ntot = ndof
+
+ALLOCATE( g_num_pp( nod, nels_pp ), g_coord_pp( nod, ndim, nels_pp ),    &
+   rest(nr,nodof+1) )
+g_num_pp = 0
+g_coord_pp = zero
+rest = 0
+
+CALL read_g_num_pp( argv, iel_start, nn, npes, numpe, g_num_pp )
+
+IF ( meshgen == 2 ) CALL abaqus2sg( element, g_num_pp )
+CALL read_g_coord_pp(argv,g_num_pp,nn,npes,numpe,g_coord_pp)
+CALL read_rest(argv,numpe,rest); timest(2)=elap_time()
+ALLOCATE( points(nip,ndim), dee(nst,nst), jac(ndim,ndim), der(ndim,nod), &
+   deriv( ndim, nod ), bee( nst, ntot ), weights( nip ), eps( nst ),     &
+   sigma(nst), storkm_pp( ntot, ntot, nels_pp ),                         &
+   pmul_pp( ntot, nels_pp ), utemp_pp(ntot,nels_pp),                     &
+   g_g_pp(ntot,nels_pp) )
 
 !*********************************************************************72
 ! CGPACK commands
@@ -181,19 +211,19 @@ call cgca_ds( cgca_space )
  END DO;  END DO elements_2
  CALL scatter(diag_precon_pp,diag_precon_tmp); DEALLOCATE(diag_precon_tmp)
  IF(numpe==1)THEN
-   OPEN(11,FILE=argv(1:nlen)//".res",STATUS='REPLACE',ACTION='WRITE')
-   WRITE(11,'(A,I7,A)') "This job ran on ",npes," processes"
-   WRITE(11,'(A,3(I12,A))') "There are ",nn," nodes", nr, &
+   OPEN(11,FILE=argv(1:nlen)//".res",STATUS='REPLACE',ACTION='write')
+   write(11,'(A,I7,A)') "This job ran on ",npes," processes"
+   write(11,'(A,3(I12,A))') "There are ",nn," nodes", nr, &
                            " restrained and ",neq," equations"
-   WRITE(11,'(A,F10.4)') "Time to read input is:",timest(2)-timest(1)
-   WRITE(11,'(A,F10.4)') "Time after setup is:",elap_time()-timest(1)
+   write(11,'(A,F10.4)') "Time to read input is:",timest(2)-timest(1)
+   write(11,'(A,F10.4)') "Time after setup is:",elap_time()-timest(1)
  END IF
 !----------------------------- get starting r ----------------------------
  IF(loaded_nodes>0) THEN
    ALLOCATE(node(loaded_nodes),val(ndim,loaded_nodes)); node=0; val=zero
    CALL read_loads(argv,numpe,node,val)
    CALL load(g_g_pp,g_num_pp,node,val,r_pp(1:)); q=SUM_P(r_pp(1:))
-   IF(numpe==1) WRITE(11,'(A,E12.4)') "The total load is:",q
+   IF(numpe==1) write(11,'(A,E12.4)') "The total load is:",q
    DEALLOCATE(node,val)
  END IF
  DEALLOCATE(g_g_pp); diag_precon_pp=1._iwp/diag_precon_pp
@@ -216,10 +246,10 @@ call cgca_ds( cgca_space )
    IF(converged.OR.iters==limit)EXIT
  END DO iterations
  IF(numpe==1)THEN
-   WRITE(11,'(A,I6)')"The number of iterations to convergence was ",iters
-   WRITE(11,'(A,F10.4)')"Time to solve equations was  :",                &
+   write(11,'(A,I6)')"The number of iterations to convergence was ",iters
+   write(11,'(A,F10.4)')"Time to solve equations was  :",                &
                          elap_time()-timest(3)  
-   WRITE(11,'(A,E12.4)')"The central nodal displacement is :",xnew_pp(1)
+   write(11,'(A,E12.4)')"The central nodal displacement is :",xnew_pp(1)
  END IF
  DEALLOCATE(p_pp,r_pp,x_pp,u_pp,d_pp,diag_precon_pp,storkm_pp,pmul_pp) 
 !--------------- recover stresses at centroidal gauss point --------------
@@ -228,22 +258,22 @@ call cgca_ds( cgca_space )
 
  ALLOCATE(eld_pp(ntot,nels_pp)); eld_pp=zero; points=zero; nip=1; iel=1
  CALL gather(xnew_pp(1:),eld_pp); DEALLOCATE(xnew_pp)
- IF(numpe==1)WRITE(11,'(A)')"The Centroid point stresses for element 1 are"
+ IF(numpe==1)write(11,'(A)')"The Centroid point stresses for element 1 are"
  gauss_pts_2: DO i=1,nip
    CALL shape_der(der,points,i); jac=MATMUL(der,g_coord_pp(:,:,iel))
    CALL invert(jac); deriv=MATMUL(jac,der); CALL beemat(bee,deriv)
    eps=MATMUL(bee,eld_pp(:,iel)); sigma=MATMUL(dee,eps)
    IF(numpe==1.AND.i==1) THEN
-     WRITE(11,'(A,I5)')"Point ",i ; WRITE(11,'(6E12.4)') sigma
+     write(11,'(A,I5)')"Point ",i ; write(11,'(6E12.4)') sigma
    END IF
  END DO gauss_pts_2; DEALLOCATE(g_coord_pp)
 !------------------------ write out displacements ------------------------
  CALL calc_nodes_pp(nn,npes,numpe,node_end,node_start,nodes_pp)
- IF(numpe==1) THEN;  WRITE(ch,'(I6.6)') numpe
+ IF(numpe==1) THEN;  write(ch,'(I6.6)') numpe
    OPEN(12,file=argv(1:nlen)//".ensi.DISPL-"//ch,status='replace',       &
      action='write')
-   WRITE(12,'(A)') "Alya Ensight Gold --- Vector per-node variable file"
-   WRITE(12,'(A/A/A)') "part", "     1","coordinates"
+   write(12,'(A)') "Alya Ensight Gold --- Vector per-node variable file"
+   write(12,'(A/A/A)') "part", "     1","coordinates"
  END IF
  ALLOCATE(disp_pp(nodes_pp*ndim),temp(nodes_pp)); disp_pp=zero; temp=zero
  CALL scatter_nodes(npes,nn,nels_pp,g_num_pp,nod,ndim,nodes_pp,          &
@@ -252,7 +282,7 @@ call cgca_ds( cgca_space )
    DO j=1,nodes_pp; k=i+(ndim*(j-1)); temp(j)=disp_pp(k); END DO
    CALL dismsh_ensi_p(12,1,nodes_pp,npes,numpe,1,temp)
  END DO ; IF(numpe==1) CLOSE(12)
- IF(numpe==1) WRITE(11,'(A,F10.4)')"This analysis took  :",              &
+ IF(numpe==1) write(11,'(A,F10.4)')"This analysis took  :",              &
    elap_time()-timest(1)  
  CALL SHUTDOWN() 
 END PROGRAM xx14
