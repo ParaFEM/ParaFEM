7c57d562724454856920a8de889fb52b8202fdb9
margetts <margetts@3e13d626-f9fb-40e8-89cf-c4797d9519a4>
2016-01-28 15:24:00 +0000

    Added interface to subroutine PCGVER1

--- trunk/parafem/src/programs/dev/xx18/xx18.f90
+++ trunk/parafem/src/programs/dev/xx18/xx18.f90
@@ -7,25 +7,44 @@ PROGRAM xx18
 !      
 !      See program p121
 !------------------------------------------------------------------------- 
+
 !USE mpi_wrapper  !remove comment for serial compilation
  USE precision; USE global_variables; USE mp_interface; USE input
  USE output; USE loading; USE timing; USE maths; USE gather_scatter
- USE steering; USE new_library; IMPLICIT NONE
+ USE steering; USE new_library; USE large_strain 
+ IMPLICIT NONE
+
 ! neq,ntot are now global variables - must not be declared
- INTEGER,PARAMETER::nodof=3,ndim=3,nst=6
- INTEGER::loaded_nodes,iel,i,j,k,iters,limit,nn,nr,nip,nod,nels,ndof,    &
-   npes_pp,node_end,node_start,nodes_pp,meshgen,partitioner,nlen
- REAL(iwp),PARAMETER::zero=0.0_iwp
- REAL(iwp)::e,v,det,tol,up,alpha,beta,q; LOGICAL::converged=.false.
- CHARACTER(LEN=50)::argv; CHARACTER(LEN=15)::element; CHARACTER(LEN=6)::ch 
-!---------------------------- dynamic arrays -----------------------------
+
+ INTEGER,PARAMETER   :: nodof=3,ndim=3,nst=6
+ INTEGER             :: loaded_nodes,iel,i,j,k,iters,limit,nn,nr,nip,nod
+ INTEGER             :: nels,ndof
+ INTEGER             :: npes_pp,node_end,node_start,nodes_pp,meshgen
+ INTEGER             :: partitioner,nlen
+ INTEGER             :: inewton ! Note - does not appear to be needed
+ REAL(iwp),PARAMETER :: zero=0.0_iwp
+ REAL(iwp)           :: rn0 ! Note - does not appear to be used
+ REAL(iwp)           :: e,v,det,tol,up,alpha,beta,q
+ LOGICAL             :: converged=.false.
+ CHARACTER(LEN=50)   :: argv
+ CHARACTER(LEN=15)   :: element
+ CHARACTER(LEN=6)    :: ch 
+
+!-------------------------------------------------------------------------
+! 1. Dynamic arrays
+!-------------------------------------------------------------------------
+
  REAL(iwp),ALLOCATABLE::points(:,:),dee(:,:),weights(:),val(:,:),        &
    disp_pp(:),g_coord_pp(:,:,:),jac(:,:),der(:,:),deriv(:,:),bee(:,:),   &
    storkm_pp(:,:,:),eps(:),sigma(:),diag_precon_pp(:),p_pp(:),r_pp(:),   &
    x_pp(:),xnew_pp(:),u_pp(:),pmul_pp(:,:),utemp_pp(:,:),d_pp(:),        &
    timest(:),diag_precon_tmp(:,:),eld_pp(:,:),temp(:)
  INTEGER,ALLOCATABLE::rest(:,:),g_num_pp(:,:),g_g_pp(:,:),node(:)
-!------------------------ input and initialisation -----------------------
+
+!-------------------------------------------------------------------------
+! 2. Input and initialisation
+!-------------------------------------------------------------------------
+
  ALLOCATE(timest(20)); timest=zero; timest(1)=elap_time()
  CALL find_pe_procs(numpe,npes); CALL getname(argv,nlen) 
  CALL read_p121(argv,numpe,e,element,limit,loaded_nodes,meshgen,nels,    &
@@ -42,7 +61,11 @@ PROGRAM xx18
    deriv(ndim,nod),bee(nst,ntot),weights(nip),eps(nst),sigma(nst),       &
    storkm_pp(ntot,ntot,nels_pp),pmul_pp(ntot,nels_pp),                   &
    utemp_pp(ntot,nels_pp),g_g_pp(ntot,nels_pp))
-!----------  find the steering array and equations per process -----------
+
+!-------------------------------------------------------------------------
+! 3. Find the steering array and equations per process
+!-------------------------------------------------------------------------
+
  CALL rearrange(rest); g_g_pp=0; neq=0
  elements_0: DO iel=1,nels_pp
    CALL find_g3(g_num_pp(:,iel),g_g_pp(:,iel),rest)
@@ -52,7 +75,11 @@ PROGRAM xx18
  ALLOCATE(p_pp(neq_pp),r_pp(neq_pp),x_pp(neq_pp),xnew_pp(neq_pp),        &
    u_pp(neq_pp),d_pp(neq_pp),diag_precon_pp(neq_pp)); diag_precon_pp=zero
  p_pp=zero;  r_pp=zero;  x_pp=zero; xnew_pp=zero; u_pp=zero; d_pp=zero
-!------ element stiffness integration and build the preconditioner -------
+
+!-------------------------------------------------------------------------
+! 4. Element stiffness integration
+!-------------------------------------------------------------------------
+
  dee=zero; CALL deemat(dee,e,v); CALL sample(element,points,weights)
  storkm_pp=zero
  elements_1: DO iel=1,nels_pp
@@ -64,6 +91,11 @@ PROGRAM xx18
                     MATMUL(MATMUL(TRANSPOSE(bee),dee),bee)*det*weights(i)   
    END DO gauss_pts_1
  END DO elements_1
+
+!-------------------------------------------------------------------------
+! 5. Build the preconditioner
+!-------------------------------------------------------------------------
+
  ALLOCATE(diag_precon_tmp(ntot,nels_pp)); diag_precon_tmp=zero
  elements_2: DO iel=1,nels_pp ; DO i=1,ndof
    diag_precon_tmp(i,iel) = diag_precon_tmp(i,iel)+storkm_pp(i,i,iel)
@@ -77,7 +109,11 @@ PROGRAM xx18
    WRITE(11,'(A,F10.4)') "Time to read input is:",timest(2)-timest(1)
    WRITE(11,'(A,F10.4)') "Time after setup is:",elap_time()-timest(1)
  END IF
-!----------------------------- get starting r ----------------------------
+
+!-------------------------------------------------------------------------
+! 6. Get starting r
+!-------------------------------------------------------------------------
+
  IF(loaded_nodes>0) THEN
    ALLOCATE(node(loaded_nodes),val(ndim,loaded_nodes)); node=0; val=zero
    CALL read_loads(argv,numpe,node,val)
@@ -86,32 +122,54 @@ PROGRAM xx18
    DEALLOCATE(node,val)
  END IF
  DEALLOCATE(g_g_pp); diag_precon_pp=1._iwp/diag_precon_pp
- d_pp=diag_precon_pp*r_pp; p_pp=d_pp; x_pp=zero
+
+!-------------------------------------------------------------------------
+! 7. Preconditioned conjugate gradient solver
+!-------------------------------------------------------------------------
+
+! d_pp=diag_precon_pp*r_pp; p_pp=d_pp; x_pp=zero
+
+ timest(3) = elap_time()
+ iters     = 0
+ x_pp      = zero
+ inewton   = 1    ! must be one in this program
+ rn0       = zero ! note rn0 does not appear to be used in XX15
+
+ CALL PCG_VER1(inewton,limit,tol,storkm_pp,r_pp(1:),diag_precon_pp(1:), &
+               rn0,x_pp,iters)
+
+ xnew_pp   = x_pp
+
 !--------------------- preconditioned cg iterations ----------------------
- iters=0; timest(3)=elap_time()
- iterations: DO 
-   iters=iters+1; u_pp=zero; pmul_pp=zero; utemp_pp=zero
-   CALL gather(p_pp,pmul_pp)
-   elements_3: DO iel=1,nels_pp
-     utemp_pp(:,iel) = MATMUL(storkm_pp(:,:,iel),pmul_pp(:,iel))
+! iters=0; timest(3)=elap_time()
+! iterations: DO 
+!   iters=iters+1; u_pp=zero; pmul_pp=zero; utemp_pp=zero
+!   CALL gather(p_pp,pmul_pp)
+!   elements_3: DO iel=1,nels_pp
+!     utemp_pp(:,iel) = MATMUL(storkm_pp(:,:,iel),pmul_pp(:,iel))
 !    CALL dgemv('n',ntot,ntot,1.0_iwp,storkm_pp(:,:,iel),ntot,           &
 !               pmul_pp(:,iel),1,0.0_iwp,utemp_pp(:,iel),1)
-   END DO elements_3 ;CALL scatter(u_pp,utemp_pp)
+!   END DO elements_3 ;CALL scatter(u_pp,utemp_pp)
 !-------------------------- pcg equation solution ------------------------
-   up=DOT_PRODUCT_P(r_pp,d_pp); alpha=up/DOT_PRODUCT_P(p_pp,u_pp)
-   xnew_pp=x_pp+p_pp*alpha; r_pp=r_pp-u_pp*alpha
-   d_pp=diag_precon_pp*r_pp; beta=DOT_PRODUCT_P(r_pp,d_pp)/up
-   p_pp=d_pp+p_pp*beta; CALL checon_par(xnew_pp,tol,converged,x_pp)    
-   IF(converged.OR.iters==limit)EXIT
- END DO iterations
+!   up=DOT_PRODUCT_P(r_pp,d_pp); alpha=up/DOT_PRODUCT_P(p_pp,u_pp)
+!   xnew_pp=x_pp+p_pp*alpha; r_pp=r_pp-u_pp*alpha
+!   d_pp=diag_precon_pp*r_pp; beta=DOT_PRODUCT_P(r_pp,d_pp)/up
+!   p_pp=d_pp+p_pp*beta; CALL checon_par(xnew_pp,tol,converged,x_pp)    
+!   IF(converged.OR.iters==limit)EXIT
+! END DO iterations
+
  IF(numpe==1)THEN
    WRITE(11,'(A,I6)')"The number of iterations to convergence was ",iters
    WRITE(11,'(A,F10.4)')"Time to solve equations was  :",                &
                          elap_time()-timest(3)  
    WRITE(11,'(A,E12.4)')"The central nodal displacement is :",xnew_pp(1)
  END IF
- DEALLOCATE(p_pp,r_pp,x_pp,u_pp,d_pp,diag_precon_pp,storkm_pp,pmul_pp) 
-!--------------- recover stresses at centroidal gauss point --------------
+!DEALLOCATE(p_pp,r_pp,x_pp,u_pp,d_pp,diag_precon_pp,storkm_pp,pmul_pp) 
+
+!-------------------------------------------------------------------------
+! 8. Recover stresses at centroidal gauss point
+!-------------------------------------------------------------------------
+
  ALLOCATE(eld_pp(ntot,nels_pp)); eld_pp=zero; points=zero; nip=1; iel=1
  CALL gather(xnew_pp(1:),eld_pp); DEALLOCATE(xnew_pp)
  IF(numpe==1)WRITE(11,'(A)')"The Centroid point stresses for element 1 are"
@@ -123,7 +181,11 @@ PROGRAM xx18
      WRITE(11,'(A,I5)')"Point ",i ; WRITE(11,'(6E12.4)') sigma
    END IF
  END DO gauss_pts_2; DEALLOCATE(g_coord_pp)
-!------------------------ write out displacements ------------------------
+
+!-------------------------------------------------------------------------
+! 9. Write out displacements
+!-------------------------------------------------------------------------
+
  CALL calc_nodes_pp(nn,npes,numpe,node_end,node_start,nodes_pp)
  IF(numpe==1) THEN;  WRITE(ch,'(I6.6)') numpe
    OPEN(12,file=argv(1:nlen)//".ensi.DISPL-"//ch,status='replace',       &
@@ -140,5 +202,7 @@ PROGRAM xx18
  END DO ; IF(numpe==1) CLOSE(12)
  IF(numpe==1) WRITE(11,'(A,F10.4)')"This analysis took  :",              &
    elap_time()-timest(1)  
+
  CALL SHUTDOWN() 
+
 END PROGRAM xx18
